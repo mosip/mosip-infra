45fae6cf4b8fbde4fb3f4de259d081f2ff72bcce
tarunchawla2 <tarun.chawla2@mindtree.com>
2020-02-18 11:14:45 +0000

    separating all modules

---
+++ deployment/sandbox/install-mosip-reg-proc.sh
@@ -0,0 +1,4 @@
+#--------------------------------------------------------
+#Before running this script, please make sure that install-mosip-kernel.sh is already executed
+#--------------------------------------------------------
+ansible-playbook  playbooks/mosip-sandbox-reg-proc-launcher.yml
