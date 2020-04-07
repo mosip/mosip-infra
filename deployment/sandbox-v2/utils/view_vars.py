f2638b7659a09e926369138fb5c513f01ce60b19
Puneet Joshi <puneet@mosip.io>
2020-04-07 09:05:37 +0530

    Utility to view all vars added.

---
+++ deployment/sandbox-v2/utils/view_vars.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# This utility lists all the vars under "defaults" of all roles
+
+import os
+import glob
+import yaml
+import pprint
+
+BASE_DIR = './'
+pp = pprint.PrettyPrinter(indent=4)
+for root, dirs, files in os.walk(BASE_DIR):
+    for d in dirs:
+        if d =='defaults':
+            var_files = glob.glob(os.path.join(root, '%s/*.yml' % d))
+            for f in var_files:
+              fd = open(f, 'rt')  
+              file_vars = yaml.load(fd)
+              print('=============================================')
+              print('ROLE: %s' % root)
+              pp.pprint(file_vars) 
+
