53a888a0b11fb22e856d309fb810eea6b465b6cf
Puneet Joshi <puneet@mosip.io>
2020-04-13 07:26:06 +0530

    [MOSIP-438] Many modules added, bug fixes.

--- deployment/sandbox-v2/app/utils/curl_auth.sh
+++ deployment/sandbox-v2/app/utils/curl_auth.sh
@@ -1,3 +1,3 @@
 # The ip
 INGRESS_IP=10.6.1.125
-curl -d '{"id": "mosip.authentication.useridPwd", "metadata": {}, "request": { "appId": "REGISTRATIONCLIENT", "password": "puneet123", "userName": "puneet" }, "requesttime": "2020-04-05T06:12:52.994Z", "version": "1.0" }' -H "Content-Type: application/json" -X POST http://$INGRESS_IP/v1/authmanager/authenticate/useridPwd 
+curl -d '{"id": "mosip.authentication.useridPwd", "metadata": {}, "request": { "appId": "mosip", "password": "puneet123", "userName": "puneet" }, "requesttime": "2020-04-12T06:12:52.994Z", "version": "1.0" }' -H "Content-Type: application/json" -X POST http://$INGRESS_IP/v1/authmanager/authenticate/useridPwd 
