d2f1d4ac6b331f33715aa71443a2867c4974edf6
Mandeep Dhiman <46880392+mandeepdhiman123@users.noreply.github.com>
2020-06-08 09:50:48 +0530

    Update mosip_repo_scanner.py

--- security/mosip_repo_scanner.py
+++ security/mosip_repo_scanner.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+rulefile = sys.argv[1]
 
 file = open('list.txt', 'r')
 count = 0
@@ -10,6 +13,6 @@ for line in file:
     name = tag[-1].split(".")
     path = line.strip()
     print("--------------------------- Scanning GitHub Repo:  " +name[0]+ " -----------------------------------")
-    os.system('trufflehog3 -v --no-entropy --no-history -r ./rules_mosip.yaml '+path+' -f html -o ./Reports_`date +%d%b%y`/mosip'+name[0]+'.html')
+    os.system('trufflehog3 -v --no-entropy --no-history -r '+rulefile+' '+path+' -f html -o ./Reports_`date +%d%b%y`/mosip'+name[0]+'.html')
 # Closing files
 file.close()
