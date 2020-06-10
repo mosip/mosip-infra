14d2993237cee21daeb02f611faa875728b2e4a3
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2020-06-10 11:48:55 +0530

    folder name corrected

--- security/mosip_repo_scanner.py
+++ security/mosip_repo_scanner.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-rulefile = sys.argv[1]
-batch = sys.argv[2]
+#rulefile = sys.argv[1]
+batch = sys.argv[1]
 file = open(batch, 'r')
 count = 0
 
@@ -12,7 +12,10 @@ for line in file:
     tag = (line.strip().split("/"))
     name = tag[-1].split(".")
     path = line.strip()
+    batchname=batch.split(".")
+    bname = batchname[0]
+    print (bname)
     print("--------------------------- Scanning GitHub Repo:  " +name[0]+ " -----------------------------------")
-    os.system('trufflehog3 -v --no-entropy --no-history -r '+rulefile+' '+path+' -f html -o ./Reports_`date +%d%b%y`/mosip'+name[0]+'.html')
+    os.system('trufflehog3 -v --no-entropy --no-history -r '+rulefile+' '+path+' -f html -o ./Reports_`date +%d%b%y`'+bname+'/mosip'+name[0]+'.html')
 # Closing files
 file.close()
