86aba346dd699b3f2bd42912826aa411d0930250
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2020-06-12 11:33:24 +0530

    added config file for central configuration of regex

--- security/mosip_repo_scanner.py
+++ security/mosip_repo_scanner.py
@@ -3,9 +3,10 @@ import sys
 
 rulefile = sys.argv[1]
 batch = sys.argv[2]
+config = sys.argv[3]
+
 file = open(batch, 'r')
 count = 0
-
 # Using for loop
 for line in file:
     count += 1
@@ -16,6 +17,6 @@ for line in file:
     bname = tag2[-1].split(".")
     batchname = bname[0]
     print("--------------------------- Scanning GitHub Repo:  " +name[0]+ " -----------------------------------")
-    os.system('trufflehog3 -v --no-entropy --no-history -r '+rulefile+' '+path+' -f html -o ./Reports_`date +%d%b%y`_'+batchname+'/mosip-'+name[0]+'.html')
+    os.system('trufflehog3 -v -c '+config+' -r '+rulefile+' '+path+' -f html -o ./Reports_`date +%d%b%y`_'+batchname+'/mosip-'+name[0]+'.html')
 # Closing files
 file.close()
