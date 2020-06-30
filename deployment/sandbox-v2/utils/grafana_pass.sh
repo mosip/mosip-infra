9a0541ad2c6b7b4ba65cd3099303a104d2a62c7d
Puneet Joshi <puneet@mosip.io>
2020-06-30 11:31:43 +0000

    [MOSIP-7724] Password enabled on grafana dashboard.

--- deployment/sandbox-v2/utils/grafana_pass.sh
+++ deployment/sandbox-v2/utils/grafana_pass.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/.bashrc
-kc1 get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}"  | base64 --decode ; echo
+kc1 get secret --namespace monitoring graf-grafana -o jsonpath="{.data.admin-password}"  | base64 --decode ; echo
