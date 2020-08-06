d6fea21e415882bb056b2cb4731f5919f2b6594b
Puneet Joshi <puneet@mosip.io>
2020-08-06 18:08:48 +0000

    [MOSIP-8599] Regproc test scripts. WIP.

--- deployment/sandbox-v2/test/regproc/test_regproc.py
+++ deployment/sandbox-v2/test/regproc/test_regproc.py
@@ -4,15 +4,10 @@ import base64
 import os
 import config as conf
 import datetime as dt
+import shutil
 from common import *
 import  jinja2 as j2
 
-def zip_packets(prefix, conf):
-    # Create dirs
-    os.makedirs(unenc_dir, exist_ok=True)
-    os.makedirs(enc_dir, exist_ok=True)
-
-  
 def update_conf(conf, mosip):
     '''
     Update the dynamic fields of conf dictonary
@@ -36,12 +31,39 @@ def template_to_packet(conf, template_path, out_dir):
         fp.write(out)
         fp.close()
 
-def make_dirs(conf):
+def zip_packets(conf):
+   '''
+   Returns paths of output zip files
+   '''
+   os.system('rm %s/*.zip' % conf.unenc_dir) # Remove any existing zip files
 
+   paths = []
+   for sub_pkt in conf.sub_pkts:
+       base_path = os.path.join(conf.unenc_dir, sub_pkt) 
+       out_zip = os.path.join(conf.unenc_dir, conf.pkt_conf['rid'] + '_' + sub_pkt)
+       r = shutil.make_archive(out_zip, 'zip', base_path)
+       paths.append(out_zip + '.zip')
+ 
+   return paths 
+
+def make_dirs(conf):
     for suffix in conf.sub_pkts:
         os.makedirs(os.path.join(conf.unenc_dir, suffix), exist_ok=True)
-        os.makedirs(os.path.join(conf.enc_dir, suffix), exist_ok=True)
-   
+
+def template_to_packets(conf):
+    for suffix in conf.sub_pkts:
+        template_to_packet(conf, os.path.join(conf.template_path,  suffix), 
+                           os.path.join(conf.unenc_dir, suffix))
+  
+def sync_packet(conf, mosip, final_zip, refid): 
+    data = open(final_zip, 'rb').read()  # data is in 'bytes'
+    phash = sha256_hash(data)
+    psize = len(data)
+    rid = conf.pkt_conf['rid'] 
+
+    r = mosip.sync_packet(rid, phash, psize, refid)
+    return r
+
 def main():
 
     make_dirs(conf)
@@ -51,32 +73,27 @@ def main():
       
     update_conf(conf, mosip)
 
-    # Convert templates
-    for suffix in conf.sub_pkts:
-        template_to_packet(conf, os.path.join(conf.template_path,  suffix), 
-                           os.path.join(conf.unenc_dir, suffix))
+    template_to_packets(conf)
+
+    zipped_pkts = zip_packets(conf)
+
+    os.system('rm %s/*.zip' % conf.enc_dir) # Remove any existing zip files
+    for zipped_pkt in zipped_pkts:
+        mosip.encrypt_packet(zipped_pkt, conf.enc_dir, os.path.basename(zipped_pkt), refid)
+
+    # Zip all the encrypted packets into single zip (which is not encrypted)
+    os.system('rm %s/*.zip' % conf.pkt_dir) # Remove any existing zip files
+    out_zip = os.path.join(conf.pkt_dir, conf.pkt_conf['rid']) 
+    final_zip = shutil.make_archive(out_zip, 'zip', conf.enc_dir) 
+
+    r = sync_packet(conf, mosip, final_zip, refid) 
+    print_response(r) 
+
     exit(0) 
 
-    '''
-    encrypt_packet(, ENCRYPTED_DIR, PACKET1, refid, token, SERVER)
-    encrypt_packet(UNENCRYPTED_PACKET2, ENCRYPTED_DIR, PACKET2, refid, token, SERVER)
-    encrypt_packet(UNENCRYPTED_PACKET3, ENCRYPTED_DIR, PACKET3, refid, token, SERVER)
-    zip_packet(RID, ENCRYPTED_DIR,  BASE_DIR)
-    
-    data = open(os.path.join(BASE_DIR, PACKET), 'rb').read()  # data is in 'bytes'
-    phash = sha256_hash(data)
-    psize = len(data)
-    
-    regid = PACKET.split('.')[0]
-    
-    sync_packet(regid, phash, psize, refid, token, SERVER) 
-    
-    packet_path = os.path.join(BASE_DIR, PACKET)
-    
     print('Uploading packet')
     r = upload_packet(packet_path, token, SERVER)
     print_response(r)
-    '''
 
 
 if __name__=="__main__":
