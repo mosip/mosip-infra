378954f9aa1ae9532da5d93477bbbf2e203b995a
Puneet Joshi <puneet@mosip.io>
2020-08-29 01:52:55 +0000

    [MOSIP-8599] Regproc test scripts enhanced.  Master key init added in commmon.py

--- deployment/sandbox-v2/test/regproc/config.py
+++ deployment/sandbox-v2/test/regproc/config.py
@@ -4,22 +4,7 @@ password = 'Techno@123'
 
 sub_pkts = ['id', 'evidence', 'optional']
 
-pkt_conf = {
-    'id_schema':  '0.1',
-    'rid': "10002100740000220200801104237",  # Created and give by server
-    'creation_date' : '2020-06-18T10:42:37.000Z', # Generate fresh
-    'prereg_id' : '', # Eg. '37029479274360'
-    'machine_id': '10074',
-    'center_id': '10002',
-    'officer_id': '110119',
-    'city': 'Kenitra',
-    'province': 'Kenitra',
-    'zone': 'Ben Mansour',
-    'region' : 'Rabat Sale Kenitra'
-}
-    
-pkt_prefix = pkt_conf['rid']
-pkt_dir = 'data/packet1'
+pkt_dir = 'data/packet_phil'
 template_dir = pkt_dir + '/template'
 unenc_dir = pkt_dir + '/unencrypted'
 enc_dir = pkt_dir + '/encrypted'
