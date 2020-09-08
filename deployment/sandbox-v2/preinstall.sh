d0e2d7756ba1c7aa65318bb4e442b07321579352
Puneet Joshi <puneet@mosip.io>
2020-09-08 00:42:38 +0000

    Preinsall script updated, documented.

--- deployment/sandbox-v2/preinstall.sh
+++ deployment/sandbox-v2/preinstall.sh
@@ -3,9 +3,10 @@
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y ansible
 sudo yum install -y tmux 
+sudo yum install -y vim
 cp $HOME/mosip-infra/deployment/sandbox-v2/utils/tmux.conf $HOME/.tmux.conf
 
-echo "alias an='ansible-playbook -i hosts.ini --ask-vault-pass -e @secrets.yml" >> $HOME/.bashsrc
+echo "alias an='ansible-playbook -i hosts.ini --ask-vault-pass -e @secrets.yml" >> $HOME/.bashrc
 echo "alias av='ansible-vault'" >> $HOME/.bashrc
 echo "alias kc1='kubectl --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
 echo "alias kc2='kubectl --kubeconfig $HOME/.kube/dmzcluster.config'" >> $HOME/.bashrc
@@ -14,4 +15,3 @@ echo "alias helm1='helm --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.ba
 echo "alias helm2='helm --kubeconfig $HOME/.kube/dmzcluster.config'" >> $HOME/.bashrc
 echo "alias helmn='helm --kubeconfig $HOME/.kube/mzcluster.config -n monitoring'" >> $HOME/.bashrc
 echo "alias kcm='kubectl -n monitoring --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
-source $HOME/.bashrc
