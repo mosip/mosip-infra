d6440889d171ddf73e5d7fbcb20ffb7372e86cac
Puneet Joshi <puneet@mosip.io>
2020-10-18 10:30:27 +0000

    Another sample packet added for basic sanity check of reg proc.

---
+++ deployment/sandbox-v2/test/regproc/data/packet2/pktconf.py
@@ -0,0 +1,14 @@
+pkt_conf = {
+    'id_schema': 0.0,
+    'rid': '',
+    'creation_date' : '',
+    'prereg_id' : '', # Eg. '37029479274360'
+    'machine_id': '10074',
+    'center_id': '10002',
+    'officer_id': '110119',
+    'city': 'Kenitra',
+    'province': 'Kenitra',
+    'zone': 'Ben Mansour',
+    'region' : 'Rabat Sale Kenitra'
+}
+
