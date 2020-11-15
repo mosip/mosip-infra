78bbd7b31adc583121dd762db3f7dadbd9c772a8
Puneet Joshi <puneet@mosip.io>
2020-11-15 13:46:56 +0000

    Minor cleanup.

--- deployment/sandbox-v2/utils/pre_reg_login.sh
+++
@@ -1,3 +0,0 @@
-# The ip
-INGRESS_IP=10.6.1.125
-curl -d '{ "id": "mosip.pre-registration.login.sendotp", "version": "1.0", "requesttime": "2020-04-06T07:24:47.605Z", "request": { "userId": "puneet.joshi007@gmail.com" } }' -H "Content-Type: application/json" -X POST http://$INGRESS_IP/preregistration/v1/login/sendOtp
