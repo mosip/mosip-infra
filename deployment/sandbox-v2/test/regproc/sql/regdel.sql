8fefe8d402300ac5f92d141619355607d69bd317
Puneet Joshi <puneet@mosip.io>
2020-12-19 06:51:33 +0000

    Test script sql updated.

--- deployment/sandbox-v2/test/regproc/sql/regdel.sql
+++ deployment/sandbox-v2/test/regproc/sql/regdel.sql
@@ -2,5 +2,6 @@ delete from reg_demo_dedupe_list;
 delete from registration_transaction;
 delete from individual_demographic_dedup;
 delete from reg_bio_ref;
+delete from reg_manual_verification;
 delete from registration;
 
