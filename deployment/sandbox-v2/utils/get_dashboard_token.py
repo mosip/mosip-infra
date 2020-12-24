01e359ed2c0335cc4e142fcf0af18436810a45b4
Nitish Srivastav <vastav2087@gmail.com>
2020-12-24 12:03:24 +0000

    [MOSIP-10856] Resolving View-only Kuberentes-dashbaord issue

--- deployment/sandbox-v2/utils/get_dashboard_token.py
+++ deployment/sandbox-v2/utils/get_dashboard_token.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 # Usage: (Example)
-# $ ./get_dashboard_token.py ~/.kube/mzcluster.config admin-user ./temp_dir/mz_dashboard_admin.token
-# $ ./get_dashboard_token.py ~/.kube/mzcluster.config view-user ./temp_dir/mz_dashboard_view.token
+# $ ./get_dashboard_token.py ~/.kube/mzcluster.config admin-user ./tmp/mz_dashboard_admin.token
+# $ ./get_dashboard_token.py ~/.kube/mzcluster.config view-user ./tmp/mz_dashboard_view.token
 
 import subprocess
 import sys
