d8eaf8a44ec719061b1185fe51f4fc3bcb0e92c7
Puneet Joshi <puneet@mosip.io>
2021-01-24 16:29:07 +0000

    [MOSIP-10061] Correction and minor cleanup.

--- deployment/sandbox-v2/utils/onboard/device/onboard.py
+++ deployment/sandbox-v2/utils/onboard/device/onboard.py
@@ -152,7 +152,7 @@ def add_spec(files):
                 r = session2.update_spec_in_masterdb(spec_id, j['name'], language)
         else:
             spec_id = 'any'
-            for language in j['language']:
+            for language in j['languages']:
                 myprint('Adding device spec in master db for (%s,%s)' % (j['name'], language))
                 r = session2.add_spec_in_masterdb(spec_id, j['name'], language)
                 _, spec_id = spec_exists_in_masterdb(j['name'], session2)
