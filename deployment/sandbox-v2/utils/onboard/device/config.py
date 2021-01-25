f42de017ddfd1190a68ae4ada9fb7baa12136582
Puneet Joshi <puneet@mosip.io>
2021-01-25 01:46:05 +0000

    [MOSIP-10061] Cleanup.

--- deployment/sandbox-v2/utils/onboard/device/config.py
+++ deployment/sandbox-v2/utils/onboard/device/config.py
@@ -15,8 +15,3 @@ partner_manager_pwd = 'mosip'
 superadmin_user = 'superadmin'
 superadmin_pwd = 'mosip'
 
-csv_device_detail = 'csv/device_detail.csv'
-csv_device_approve = 'csv/device_detail_approve.csv'
-csv_sbi = 'csv/sbi.csv'
-csv_device = 'csv/device.csv'
-csv_digital_id = 'csv/digital_id.csv'
