14b3c05c7b3e217a666a3335a923347668cbd805
Puneet Joshi <puneet@mosip.io>
2021-02-05 06:39:14 +0000

    [MOSIP-11730] Reprocess script added.

---
+++ deployment/sandbox-v2/utils/reprocess/README.md
@@ -0,0 +1,28 @@
+# Reproces packet
+
+## Context
+Registration Processor provides Reprocessor stage that processes failed packets at schedule specified by a cron job. However, if you need to process a packet manually then you may use the script provided here.
+
+## Prequisites
+Install required packets using
+```
+$ ./preinstall.sh
+```
+
+## Run
+The script takes Registration Ids (RIDs) as input. Provide RID directly as argument or via a file. Example:
+```
+$ ./reprocess.py --rid 10002100740000520210122091401
+
+OR
+
+$ ./reprocess.py --file rids.txt
+
+where rids.txt looks like:
+10002100740000520210122091401
+10001100130000120210114122006
+...
+...
+```
+
+
---
+++ deployment/sandbox-v2/utils/reprocess/api.py
@@ -0,0 +1,77 @@
+import datetime as dt
+import requests
+import json
+import base64
+import os
+import secrets
+import sys
+sys.path.insert(0, '../')
+from utils import *
+
+class MosipSession:
+    def __init__(self, server, user, pwd, appid='admin', ssl_verify=True, client_token=False):
+        self.server = server
+        self.user = user
+        self.pwd = pwd
+        self.ssl_verify = ssl_verify
+        if client_token:
+            self.token = self.auth_get_client_token(appid, self.user, self.pwd) 
+        else:
+            self.token = self.auth_get_token(appid, self.user, self.pwd) 
+      
+    def auth_get_token(self, appid, username, pwd):
+        url = '%s/v1/authmanager/authenticate/useridPwd' % self.server
+        ts = get_timestamp()
+        j = {
+            "id": "mosip.io.userId.pwd",
+            "metadata" : {},
+                "version":"1.0",
+                "requesttime": ts,
+                "request": {
+                    "appId" : appid,
+                    "userName": username,
+                    "password": pwd
+            }
+        }
+        r = requests.post(url, json = j, verify=self.ssl_verify)
+        token = read_token(r)
+        return token
+
+    def auth_get_client_token(self, appid, client_id, pwd):
+        url = '%s/v1/authmanager/authenticate/clientidsecretkey' % self.server
+        ts = get_timestamp()
+        j = {
+            'id': 'string',
+            'metadata': {},
+            'request': {
+                'appId': appid,
+                'clientId': client_id,
+                'secretKey': pwd
+            },
+            'requesttime': ts,
+            'version': '1.0'
+        }
+
+        r = requests.post(url, json = j, verify=self.ssl_verify)
+        token = read_token(r)
+        return token
+
+    def notify_securezone(self, rid):
+        '''
+        policies: dict with policies structure
+        '''
+        url = '%s/registrationprocessor/v1/securezone/notification' % self.server
+
+        cookies = {'Authorization' : self.token}
+        ts = get_timestamp()
+        j = {
+           'reg_type': 'NEW',
+           'rid': rid, 
+           'isValid': True,
+           'internalError': False,
+           'messageBusAddress': None,
+           'retryCount': None
+        }
+        r = requests.post(url, cookies=cookies, json = j, verify=self.ssl_verify)
+        r = response_to_json(r)
+        return r
