390cc656dccf4ac69f609ad0c7230a142e797cc4
Puneet Joshi <puneet@mosip.io>
2021-02-23 01:18:02 +0000

    [MOSIP-11529] Parameters read from env variables.

--- deployment/sandbox-v2/utils/onboard/idschema/config.py
+++ deployment/sandbox-v2/utils/onboard/idschema/config.py
@@ -1,8 +1,10 @@
+import os
+
 server = 'http://mzingress.sb:30080'  # Ingress
 #server = 'https://minibox.mosip.net'
 ssl_verify = True
 
 superadmin_user = 'superadmin'
-superadmin_pwd =  'mosip'
+superadmin_pwd =  os.gentenv('SUPERADMIN_PWD')
 csv_idschema = 'csv/idschema.csv'
 
