4d55071ed20abc3ba3b2384786c3b1891e6d965d
Puneet Joshi <puneet@mosip.io>
2021-03-18 10:47:26 +0530

    [MOSIP-12620] Added remaining repos.

--- build/postgres-init/docker_build.sh
+++ build/postgres-init/docker_build.sh
@@ -5,6 +5,11 @@ mkdir -p repos
 cd repos
 git clone -b $1 https://github.com/mosip/commons 
 git clone -b $1 https://github.com/mosip/pre-registration
+git clone -b $1 https://github.com/mosip/registration
+git clone -b $1 https://github.com/mosip/partner-management-services
+git clone -b $1 https://github.com/mosip/id-authentication
+git clone -b $1 https://github.com/mosip/id-repository
+git clone -b $1 https://github.com/mosip/websub
 cd ../
 docker build -t postgres-init .
 
