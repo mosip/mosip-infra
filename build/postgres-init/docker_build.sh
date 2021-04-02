95bdd074ad72a69b60e0fc69d8b284fa193b3ff6
Puneet Joshi <puneet@mosip.io>
2021-04-02 20:34:46 +0530

    [MOSIP-12620] DML_FLAG to be specified while running docker.

--- build/postgres-init/docker_build.sh
+++ build/postgres-init/docker_build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # ./docker_build.sh git_tag
-
+# TODO: Current git tag used is mosip-12620 (for current testing).  The changes in all scripts here need
+# to be merged to main stream.
+# To see the differences in the script that were made for deployment, take git diff between `develop` and `mosip-12620` # branch
 mkdir -p repos
 cd repos
 git clone -b $1 https://github.com/mosip/commons 
