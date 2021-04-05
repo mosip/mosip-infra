811c390574667ae59b1842f2a0b27175f66ffc55
Puneet Joshi <puneet@mosip.io>
2021-04-05 14:46:06 +0530

    [MOSIP-12926] Keycloak secrets copied.

--- deployment/v3/mosip/config_server/copy_secrets.sh
+++ deployment/v3/mosip/config_server/copy_secrets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 kubectl -n postgres get secret db-common-secrets -o yaml | sed 's/namespace: postgres/namespace: config-server/g' | kubectl -n config-server create -f -  
+kubectl -n keycloak get secret client-secrets -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server create -f -  
 
 
