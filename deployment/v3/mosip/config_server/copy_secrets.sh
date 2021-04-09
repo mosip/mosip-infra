891d11eb271c2b01fd84364bc57ab5a6b26d5e80
Puneet Joshi <puneet@mosip.io>
2021-04-09 21:44:16 +0530

    [MOSIP-12926] Config server procedure updated.

--- deployment/v3/mosip/config_server/copy_secrets.sh
+++ deployment/v3/mosip/config_server/copy_secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-kubectl -n postgres get secret db-common-secrets -o yaml | sed 's/namespace: postgres/namespace: config-server/g' | kubectl -n config-server create -f -  
-kubectl -n keycloak get secret keycloak-client-secrets -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server create -f -  
-kubectl -n keycloak get secret keycloak -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server create -f -  
+#kubectl -n postgres get secret db-common-secrets -o yaml | sed 's/namespace: postgres/namespace: config-server/g' | kubectl -n config-server create -f -  
+kubectl --kubeconfig ~/.kube/iam_config -n keycloak get secret keycloak -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server apply -f -  
+kubectl --kubeconfig ~/.kube/iam_config -n keycloak get secret keycloak-client-secrets -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server apply -f -  
 
 
