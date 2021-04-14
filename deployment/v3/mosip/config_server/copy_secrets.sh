2310c0df0a842b12124b770d0c10c69781169832
Puneet Joshi <puneet@mosip.io>
2021-04-14 10:01:04 +0530

    [MOSIP-13230] Updated for softhsm.

--- deployment/v3/mosip/config_server/copy_secrets.sh
+++ deployment/v3/mosip/config_server/copy_secrets.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-#kubectl -n postgres get secret db-common-secrets -o yaml | sed 's/namespace: postgres/namespace: config-server/g' | kubectl -n config-server create -f -  
+# Postgres
+kubectl -n postgres get secret db-common-secrets -o yaml | sed 's/namespace: postgres/namespace: config-server/g' | kubectl -n config-server create -f -  
+
+# Keycloak
 kubectl --kubeconfig ~/.kube/iam_config -n keycloak get secret keycloak -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server apply -f -  
 kubectl --kubeconfig ~/.kube/iam_config -n keycloak get secret keycloak-client-secrets -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server apply -f -  
+
+# Softhsm
+kubectl -n softhsm get secret softhsm-kernel -o yaml | sed 's/namespace: softhsm/namespace: config-server/g' | kubectl -n config-server create -f -  
+# If IDA has been installed
+kubectl -n softhsm get secret softhsm-ida -o yaml | sed 's/namespace: softhsm/namespace: config-server/g' | kubectl -n config-server create -f -  
 
 
