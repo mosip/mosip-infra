ebeb4672ab7cd691571e30d5bf5f52f23b95aed8
Puneet Joshi <puneet@mosip.io>
2021-04-21 15:57:29 +0530

    [MOSIP-11894] CM copy script enhanced.

--- deployment/v3/mosip/kernel/copy_cm.sh
+++ deployment/v3/mosip/kernel/copy_cm.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 # Copy configmaps from other namespaces
-# DST_NS: Destination (current) namespace 
+# DST_NS: Destination namespace 
+
 COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=keymanager
+DST_NS=kernel
 
 $COPY_UTIL global default $DST_NS 
 $COPY_UTIL artifactory-share artifactory $DST_NS 
 $COPY_UTIL config-server-share config-server $DST_NS
-$COPY_UTIL softhsm-kernel-share softhsm $DST_NS
-
-
-
-
