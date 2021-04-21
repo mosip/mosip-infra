aaad6928fa9f5e26d124e978b268c6a8726a01fe
Puneet Joshi <puneet@mosip.io>
2021-04-21 09:26:12 +0530

    [MOSIP-12620] Get password script enhanced.

--- deployment/v3/external/postgres/cluster/get_pwd.sh
+++ deployment/v3/external/postgres/cluster/get_pwd.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Get postgres password
-echo Password: $(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+echo postgres: $(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+echo app-admin: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-appadmin-password}" | base64 --decode)
+echo db-admin: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-dbadmin-password}" | base64 --decode)
+echo db-user: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-dbuser-password}" | base64 --decode)
+echo sysadmin-user: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-sysadmin-password}" | base64 --decode)
