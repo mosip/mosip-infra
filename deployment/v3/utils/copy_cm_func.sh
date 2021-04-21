e2a1b73c31df9aeda0776a564af6cc2ba2bb6f51
Puneet Joshi <puneet@mosip.io>
2021-04-21 16:17:01 +0530

    [MOSIP-11894] Configmap/secret copy scripts enhanced.

--- deployment/v3/utils/copy_cm_func.sh
+++ deployment/v3/utils/copy_cm_func.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
-# Copy configmap from one namespace to another
-# "cluster config path" is only needed while copying CMs from other clusters. Example: `~/.kube/keycloak_config`
-# ./copy_cm_func.sh <configmap name> <source namespace> <destination namespace> [source cluster config]
-if [ ! -z $4 ]
+# Copy configmap and secret from one namespace to another
+# ./copy_cm_func.sh <resource> <configmap_name> <source_namespace> <destination_namespace> [source_cluster_config]
+# Parameters:
+#   resource: configmap|secret
+#   source_cluster_config (optional): only needed while copying resources from other clusters. Example: `~/.kube/keycloak_config`
+
+if [ $1 == "configmap" ]
 then
-  KC_SOURCE="kubectl --kubeconfig $4"
+  RESOURCE=configmap
+elif [ $1 == "secret" ]
+then
+  RESOURCE=secret
+else
+  echo "Incorrect resource $1. Exiting.."
+  exit 1
+fi
+
+if [ ! -z $5 ]
+then
+  KC_SOURCE="kubectl --kubeconfig $5"
 else
   KC_SOURCE=kubectl
 fi
-kubectl -n $3 delete --ignore-not-found=true cm $1
-$KC_SOURCE -n $2 get cm $1 -o yaml | sed "s/namespace: $2/namespace: $3/g" | kubectl -n $3 create -f -  
+kubectl -n $4 delete --ignore-not-found=true $RESOURCE $2
+$KC_SOURCE -n $3 get $RESOURCE $2 -o yaml | sed "s/namespace: $3/namespace: $4/g" | kubectl -n $4 create -f -  
+
 
 
 
