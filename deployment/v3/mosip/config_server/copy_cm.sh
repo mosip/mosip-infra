23259e149954e1d002bbb8d9662e98aaa1dc2e2b
Puneet Joshi <puneet@mosip.io>
2021-04-21 16:17:01 +0530

    [MOSIP-11894] Configmap/secret copy scripts enhanced.

--- deployment/v3/mosip/config_server/copy_cm.sh
+++ deployment/v3/mosip/config_server/copy_cm.sh
@@ -4,12 +4,8 @@
 COPY_UTIL=../../utils/copy_cm_func.sh
 DST_NS=config-server
 
-$COPY_UTIL global default $DST_NS 
-$COPY_UTIL keycloak-host keycloak $DST_NS ~/.kube/iam_config
-
-## Keycloak
-#kubectl -n config-server delete --ignore-not-found=true cm keycloak-host
-#kubectl --kubeconfig ~/.kube/iam_config -n keycloak get cm keycloak-host -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server create -f -  
+$COPY_UTIL configmap global default $DST_NS 
+$COPY_UTIL configmap keycloak-host keycloak $DST_NS ~/.kube/iam_config
 
 
 
--- deployment/v3/mosip/config_server/copy_secrets.sh
+++ deployment/v3/mosip/config_server/copy_secrets.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
-# Postgres
-kubectl -n config-server delete --ignore-not-found=true secret db-common-secrets 
-kubectl -n postgres get secret db-common-secrets -o yaml | sed 's/namespace: postgres/namespace: config-server/g' | kubectl -n config-server create -f -  
+# Copy secrets from other namespaces
+# DST_NS: Destination namespace 
+COPY_UTIL=../../utils/copy_cm_func.sh
+DST_NS=config-server
 
-# Keycloak
-kubectl -n config-server delete --ignore-not-found=true secret keycloak
-kubectl --kubeconfig ~/.kube/iam_config -n keycloak get secret keycloak -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server create -f -  
-
-kubectl -n config-server delete --ignore-not-found=true secret keycloak-client-secrets
-kubectl --kubeconfig ~/.kube/iam_config -n keycloak get secret keycloak-client-secrets -o yaml | sed 's/namespace: keycloak/namespace: config-server/g' | kubectl -n config-server create -f -  
-
-# Softhsm
-kubectl -n config-server delete --ignore-not-found=true secret softhsm-kernel
-kubectl -n softhsm get secret softhsm-kernel -o yaml | sed 's/namespace: softhsm/namespace: config-server/g' | kubectl -n config-server create -f -  
-
-# If IDA has been installed
-#kubectl -n softhsm get secret softhsm-ida -o yaml | sed 's/namespace: softhsm/namespace: config-server/g' | kubectl -n config-server create -f -  
+$COPY_UTIL secret db-common-secrets postgres $DST_NS 
+$COPY_UTIL secret keycloak keycloak $DST_NS ~/.kube/iam_config
+$COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS ~/.kube/iam_config
+$COPY_UTIL secret softhsm-kernel softhsm $DST_NS 
+# If IDA is installed
+#$COPY_UTIL secret softhsm-ida softhsm $DST_NS 
 
 
