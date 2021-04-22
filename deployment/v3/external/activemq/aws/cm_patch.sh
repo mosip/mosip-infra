a3e02925db2d29fc93a9c82ec34ccb0adafcc2ae
Puneet Joshi <puneet@mosip.io>
2021-04-22 14:54:54 +0530

    [MOSIP-12232] Enhanced.

--- deployment/v3/external/activemq/aws/cm_patch.sh
+++ deployment/v3/external/activemq/aws/cm_patch.sh
@@ -3,5 +3,4 @@
 # installed 
 
 ACTIVEMQ_HOST=$(kubectl -n activemq get svc activemq-activemq-artemis -o "jsonpath={.status.loadBalancer.ingress[0].hostname}")
-echo $ACTIVEMQ_HOST
 kubectl -n activemq  create configmap activemq-activemq-artemis-share --from-literal=activemq-host=$ACTIVEMQ_HOST --dry-run=client -o yaml | kubectl apply -f -
