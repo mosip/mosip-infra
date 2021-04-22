d5002d048fbbe8f6b82f6a0900507276aa0727ef
Puneet Joshi <puneet@mosip.io>
2021-04-22 10:20:33 +0530

    [MOSIP-13000] Instance type updated.

--- deployment/v3/cluster/aws/create.sh
+++ deployment/v3/cluster/aws/create.sh
@@ -3,7 +3,7 @@ eksctl create cluster \
 --name MOSIPCluster \
 --nodegroup-name mosip-worker-nodes \
 --nodes 2 \
---instance-types=m5a.xlarge \
+--instance-types=r5a.large \
 --ssh-access \
 --ssh-public-key rancher \
 --managed
