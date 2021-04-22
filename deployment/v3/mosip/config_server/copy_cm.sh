b9f1bbc2093b82b2e2544f3d24f9ba1a10e2c851
Puneet Joshi <puneet@mosip.io>
2021-04-22 14:56:57 +0530

    [MOSIP-12232] Activemq params added.

--- deployment/v3/mosip/config_server/copy_cm.sh
+++ deployment/v3/mosip/config_server/copy_cm.sh
@@ -6,6 +6,7 @@ DST_NS=config-server
 
 $COPY_UTIL configmap global default $DST_NS 
 $COPY_UTIL configmap keycloak-host keycloak $DST_NS ~/.kube/iam_config
+$COPY_UTIL configmap activemq-activemq-artemis-share activemq $DST_NS
 
 
 
--- deployment/v3/mosip/config_server/copy_secrets.sh
+++ deployment/v3/mosip/config_server/copy_secrets.sh
@@ -8,6 +8,7 @@ $COPY_UTIL secret db-common-secrets postgres $DST_NS
 $COPY_UTIL secret keycloak keycloak $DST_NS ~/.kube/iam_config
 $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS ~/.kube/iam_config
 $COPY_UTIL secret softhsm-kernel softhsm $DST_NS 
+$COPY_UTIL secret activemq-activemq-artemis activemq $DST_NS 
 # If IDA is installed
 #$COPY_UTIL secret softhsm-ida softhsm $DST_NS 
 
