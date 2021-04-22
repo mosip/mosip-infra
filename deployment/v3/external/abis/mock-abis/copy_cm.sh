96e66011c9b39b51b75417f052e3c93969c1f597
Puneet Joshi <puneet@mosip.io>
2021-04-22 15:00:54 +0530

    [MOSIP-13477] Mock ABIS install procedure added.

---
+++ deployment/v3/external/abis/mock-abis/copy_cm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Copy configmaps from other namespaces
+# DST_NS: Destination namespace 
+COPY_UTIL=../../../utils/copy_cm_func.sh
+DST_NS=abis
+
+$COPY_UTIL configmap global default $DST_NS 
+$COPY_UTIL configmap config-server-share config-server $DST_NS 
+
+
+
+
