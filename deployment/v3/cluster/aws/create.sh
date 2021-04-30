f96e415a50f97058d95f7f8e79c17801864d8422
Puneet Joshi <puneet@mosip.io>
2021-04-30 14:59:41 +0530

    [MOSIP-13000] AWS instance spec updated.

--- deployment/v3/cluster/aws/create.sh
+++ deployment/v3/cluster/aws/create.sh
@@ -3,7 +3,7 @@ eksctl create cluster \
 --name MOSIPCluster \
 --nodegroup-name mosip-worker-nodes \
 --nodes 2 \
---instance-types=r5a.large \
+--instance-types=t5a.xlarge \
 --ssh-access \
 --ssh-public-key rancher \
 --managed
