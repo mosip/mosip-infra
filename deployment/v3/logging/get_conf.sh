1ad5d6a612588fb7a8657ad84826c0fc806ef085
Puneet Joshi <puneet@mosip.io>
2021-05-11 21:02:59 +0530

    [MOSIP-13637] Minor doc update.

--- deployment/v3/logging/get_conf.sh
+++ deployment/v3/logging/get_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Get postgres password
+# Get fluentd.conf  and fluent.conf
 echo fluentd.conf: $(kubectl get secret --namespace cattle-logging-system rancher-logging-eks-fluentd-app -o jsonpath="{.data.fluentd\.conf}" | base64 --decode)
 echo ''
 echo fluent.conf: $(kubectl get secret --namespace cattle-logging-system rancher-logging-eks-fluentd -o jsonpath="{.data.fluent\.conf}" | base64 --decode)
