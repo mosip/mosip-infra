20f595f5bbec79fc7bbad94ede0efe8b81a75cf8
Puneet Joshi <puneet@mosip.io>
2021-05-13 10:56:32 +0530

    [MOSIP-12999] Scripts updated.

--- deployment/v3/external/iam/get_pwd.sh
+++ deployment/v3/external/iam/get_pwd.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # Get admin password
 echo Keycloak admin password: $(kubectl --kubeconfig ~/.kube/iam_config get secret --namespace keycloak keycloak -o jsonpath="{.data.admin-password}" | base64 --decode)
+echo Keycloak prereg client password: $(kubectl --kubeconfig ~/.kube/iam_config get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.preregistration_mosip_prereg_client_secret}" | base64 --decode)
+echo Keycloak postgresql password: $(kubectl --kubeconfig ~/.kube/iam_config get secret --namespace keycloak keycloak-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+echo Keycloak postgresql postgres password: $(kubectl --kubeconfig ~/.kube/iam_config get secret --namespace keycloak keycloak-postgresql -o jsonpath="{.data.postgresql-postgres-password}" | base64 --decode)
