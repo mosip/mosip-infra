0675111f3f108fec34ce9681ae23cb142bfdbbc7
Puneet Joshi <puneet@mosip.io>
2021-05-16 18:52:14 +0530

    Doc updated

--- deployment/v3/external/iam/update_secret.sh
+++ deployment/v3/external/iam/update_secret.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # If you update admin password via console, call this script to update the secret in kubernetes.
-# Parameter: new admin password
+# Usage:
+# ./update_secrets.sh <new_admin_password>
 kubectl -n keycloak create secret generic keycloak --from-literal=admin-password=$1 --dry-run=client -o yaml | kubectl -n keycloak  apply -f -
