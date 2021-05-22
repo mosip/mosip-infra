51049d53e21d76f838846ca34bbd9b42963d3715
ckm007 <chandrakeshavmishra@gmail.com>
2021-05-22 09:52:16 +0000

    [MOSIP-13711] updated latest packetmanager change for 1.1.4.3

--- deployment/sandbox-v2/preinstall.sh
+++ deployment/sandbox-v2/preinstall.sh
@@ -17,3 +17,5 @@ echo "alias helm1='helm --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.ba
 echo "alias helm2='helm --kubeconfig $HOME/.kube/dmzcluster.config'" >> $HOME/.bashrc
 echo "alias helmm='helm --kubeconfig $HOME/.kube/mzcluster.config -n monitoring'" >> $HOME/.bashrc
 echo "alias kcm='kubectl -n monitoring --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
+echo "alias kcp='kubectl -n packetmanager --kubeconfig $HOME/.kube/mzcluster.config'" >> $HOME/.bashrc
+echo "alias helmp='helm --kubeconfig $HOME/.kube/mzcluster.config -n packetmanager'" >> $HOME/.bashrc
