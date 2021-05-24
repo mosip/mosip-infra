ccd51ab96dfddd8dedfa1c1aa3621fe8732cd357
Puneet Joshi <puneet@mosip.io>
2021-05-24 06:19:21 +0530

    [MOSIP-11894] Minor script update.

--- deployment/v3/mosip/kernel/install_all.sh
+++ deployment/v3/mosip/kernel/install_all.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 # Installs all kernel helm charts 
 helm repo update
+echo "Installing keymanager"
 helm -n keymanager install keymanager mosip/keymanager
+echo "Installing authmanager"
 helm -n kernel install authmanager mosip/authmanager
+echo "Installing auditmanager"
 helm -n kernel install auditmanager mosip/auditmanager
+echo "Installing idgenerator"
 helm -n kernel install idgenerator mosip/idgenerator  
+echo "Installing masterdata"
 helm -n kernel install masterdata mosip/masterdata
+echo "Installing otpmanager"
 helm -n kernel install otpmanager mosip/otpmanager
+echo "Installing pridgenerator"
 helm -n kernel install pridgenerator mosip/pridgenerator
+echo "Installing ridgenerator"
 helm -n kernel install ridgenerator mosip/ridgenerator
+echo "Installing syncdata"
 helm -n kernel install syncdata mosip/syncdata
