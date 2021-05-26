723aea62c3b9f86e53d11857fb985f07271e1001
Puneet Joshi <puneet@mosip.io>
2021-05-26 13:14:36 +0530

    [MOSIP-12620] Minor updates.

--- deployment/v3/external/postgres/cluster/istio/get_pwd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Get postgres password
-echo postgres: $(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
-echo app-admin: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-appadmin-password}" | base64 --decode)
-echo db-admin: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-dbadmin-password}" | base64 --decode)
-echo db-user: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-dbuser-password}" | base64 --decode)
-echo sysadmin-user: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-sysadmin-password}" | base64 --decode)
