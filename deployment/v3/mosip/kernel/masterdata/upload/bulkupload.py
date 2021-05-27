73d8c4c62f074494d06eb964a9abe3a57757819b
Lalith Kota <kotalalith@gmail.com>
2021-05-27 19:46:56 +0530

    [MOSIP-14336]Masterdata upload utility slight changes

--- deployment/v3/mosip/kernel/masterdata/upload/bulkupload.py
+++ deployment/v3/mosip/kernel/masterdata/upload/bulkupload.py
@@ -7,6 +7,7 @@ import csv
 import json
 import pandas as pd
 import config as conf
+import tempfile
 from utils import *
 
 def get_order_from_list(files_file_name):
@@ -20,36 +21,37 @@ def get_order_from_list(files_file_name):
     except:
         sys.exit('Table Order file not found ' + files_file_name + ' %tb')
     return table_order
-    # return [l.strip() for l in open(files_file_name, 'r') if not l.strip().startswith('#')]
 
 def bulk_upload_csv_files_to_masterdata(files, table_order):
-    try:
-        os.mkdir('tmp')
-    except:
-        pass
-
     session = MosipSession(conf.server, conf.superadmin_user, conf.superadmin_pwd, ssl_verify=conf.ssl_verify)
     for f in table_order:
         for fi in files:
             if f==os.path.basename(fi).split('.')[0]:
-                # myprint(os.path.splitext(os.path.basename(f)))
-                pd.read_excel(fi).to_csv('tmp/tmp.csv',index=False)
-                r=session.bulk_upload('masterdata','tmp/tmp.csv','insert',f)
                 myprint(fi)
-                myprint(response_to_json(r))
-                os.remove('tmp/tmp.csv')
-                break
-    return 0
+                fi_temp = os.path.basename(fi).split('.')
+                fi_temp[-1] = 'csv'
+                fi_temp_name = os.path.join(tempfile.gettempdir(),'.'.join(fi_temp))
+                if os.path.exists(fi_temp_name):
+                    os.remove(fi_temp_name)
+                pd.read_excel(fi,keep_default_na=False).to_csv(fi_temp_name,index=False)
+                r=session.bulk_upload('masterdata',fi_temp_name,'insert',f)
+                r=response_to_json(r)
+                myprint(r)
+                if len(r['errors'])!=0:
+                    sys.exit(10)
+                if r['response']['status']!='COMPLETED':
+                    if r['response']['statusDescription'].find('Duplicate Record') == -1:
+                        sys.exit(11)
+                os.remove(fi_temp_name)
 
 def args_parse():
-   parser = argparse.ArgumentParser()
-   #parser.add_argument('action', help='type|spec|machine')
-   parser.add_argument('path', help='directory containing all the tables in xlsx format')
-   parser.add_argument('order', help='file that contains the table upload order')
-   parser.add_argument('--server', type=str, help='Full url to point to the server.  Setting this overrides server specified in config.py')
-   parser.add_argument('--disable_ssl_verify', help='Disable ssl cert verification while connecting to server', action='store_true')
-   args = parser.parse_args()
-   return args, parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='directory containing all the tables in xlsx format')
+    parser.add_argument('order', help='file that contains the table upload order')
+    parser.add_argument('--server', type=str, help='Full url to point to the server.  Setting this overrides server specified in config.py')
+    parser.add_argument('--disable_ssl_verify', help='Disable ssl cert verification while connecting to server', action='store_true')
+    args = parser.parse_args()
+    return args, parser
 
 def main():
 
@@ -67,12 +69,6 @@ def main():
     init_logger('full', 'a', './out.log', level=logging.INFO)  # Append mode
     init_logger('last', 'w', './last.log', level=logging.INFO, stdout=False)  # Just record log of last run
 
-    # if args.action == 'type':
-    #     add_type(files)
-    # if args.action == 'spec':
-    #     add_spec(files)
-    # if args.action == 'machine':
-    #     add_machine(files)
     bulk_upload_csv_files_to_masterdata(files,table_order)
 
 if __name__=="__main__":
