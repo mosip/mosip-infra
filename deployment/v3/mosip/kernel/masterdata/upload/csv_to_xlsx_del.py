4282baf42b1832520f20dba5d026fef034787e65
nitish2087 <vastav2087@gmail.com>
2021-05-28 16:57:01 +0530

    [MOSIP-14336] Updating Masterdata Utility

--- deployment/v3/mosip/kernel/masterdata/upload/csv_to_xlsx_del.py
+++ deployment/v3/mosip/kernel/masterdata/upload/csv_to_xlsx_del.py
@@ -1,4 +1,6 @@
-# Python script to convert csv files to excel files.
+# Python script to convert csv files to excel files by removing:
+#   - optional columns(cr_by | cr_dtimes | upd_by | upd_dtimes | is_deleted | del_dtimes)
+#   - Arabic language
 
 #!/usr/local/bin/python3
 
@@ -13,6 +15,19 @@ def csv_to_xlsx(path_to_csv_dir, path_to_xlsx_dir):
     files_xlsx = path_to_xlsx_dir
     for i in range(len(files)):
         data = pd.read_csv(files[i], error_bad_lines=False)
+        files[i] = files[i].replace("master-","") #removing prefix-master from csv files
+        # to delete columns
+        drop_list = ['cr_by', 'cr_dtimes', 'upd_by', 'upd_dtimes', 'is_deleted', 'del_dtimes']
+        for j in drop_list:
+            if j in data.columns:
+                data.drop(j, inplace=True, axis=1)
+
+        #to delete lang_code:ara
+        try:
+            data = data.set_index('lang_code')
+            data = data.drop('ara', axis=0)
+        except:
+            pass 
         filename = (files[i].replace('csv','')+'xlsx').replace(path_to_csv_dir+'/', '')
         path = os.path.join(files_xlsx, os.path.basename(filename))
         data.to_excel(path)
