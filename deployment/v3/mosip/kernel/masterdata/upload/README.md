a6f91c65952f960160522699dc0b09e85a83e4ed
Lalith Kota <kotalalith@gmail.com>
2021-06-02 15:22:00 +0530

    [MOSIP-14369] Creating sample masterdata and adding and updating new scripts

--- deployment/v3/mosip/kernel/masterdata/upload/README.md
+++ deployment/v3/mosip/kernel/masterdata/upload/README.md
@@ -1,10 +1,42 @@
-## 1. To bulkupload
-```
-$ chmod +x bulkupload.py
-$ ./bulkupload.py [path-to-xlsx-dir] [table-order-file]
-```
-Make sure to change the first line of `bulkupload.py` to the correct python3 location
+# Masterdata Upload Utility
 
-This will bulkupload all xlsx files in order of `[table-order-file]` from `[path-to-xlsx-dir]` according to their file names.
+It is suggested to not edit the csv files directly. Instead use the xlsx files from the provided directory and "Save as CSV" into the csv directory.
 
-This uses the /v1/admin/bulkupload api.
+## 1. To bulkupload using api
+- ```
+  $ export IAM_USERNAME=<username>
+  $ export IAM_PASSWORD=<password>
+  $ chmod +x bulkupload.py
+  $ ./bulkupload.py [path-to-csv-dir] [table-order-file]
+  ```
+- Make sure to change the first line of `bulkupload.py` to the correct python3 location.
+- This will bulkupload all csv files in order of `[table-order-file]` from `[path-to-csv-dir]` according to their file names, using the `/v1/admin/bulkupload` api.
+
+## 2. To bulkupload directly to db using sql
+
+- It is recommended to use the above script in at all cases, unless absolutely required.
+- ```
+  $ chmod +x bulkupload.py
+  $ export IAM_USERNAME=<username>
+  $ export DB_USER=<db_username>
+  $ export DB_PWD=<db_password>
+  $ export DB_HOST=<db_host>
+  $ export DB_PORT=<db_port>
+  $ export DB_NAME=<db_name>
+  $ ./bulkupload_using_sql.py [path-to-csv-dir] [table-order-file]
+  ```
+- Make sure to change the first line of `bulkupload_using_sql.py` to the correct python3 location.
+- This will bulkupload all csv files in order of `[table-order-file]` from `[path-to-csv-dir]` according to their file names, directly to database, using sql "insert".
+
+## 3. Miscellaneous one-time scripts
+#### 3.1 csv_del_data.py
+- Use this script to delete unwanted data in the csvs, like unwanted rows and columns.
+- ```
+  $ ./csv_del_data.py [original-csv-dir] [output-empty-csv-dir]
+  ```
+
+#### 3.2 csv_to_xlsx.py
+- Use this script to convert all csv files from a directory to xlsx files.
+- ```
+  $ ./csv_to_xlsx.py [original-csv-dir] [output-empty-xlsx-dir]
+  ```
--- deployment/v3/mosip/kernel/masterdata/upload/bulkupload_using_sql.py
+++ deployment/v3/mosip/kernel/masterdata/upload/bulkupload_using_sql.py
@@ -3,10 +3,13 @@
 import sys
 import argparse
 from api import *
+from utils import *
 import csv
 import json
+import pandas as pd
 import config as conf
-from utils import *
+from datetime import datetime as dt
+from sqlalchemy import create_engine
 
 def get_order_from_list(files_file_name):
     table_order = []
@@ -20,39 +23,27 @@ def get_order_from_list(files_file_name):
         sys.exit('Table Order file not found ' + files_file_name + ' %tb')
     return table_order
 
-def bulk_upload_csvs_using_api(files, table_order):
-    session = MosipSession(conf.server, conf.superadmin_user, conf.superadmin_pwd, ssl_verify=conf.ssl_verify)
+def bulk_upload_csvs_using_sql(files, table_order):
+    # with psycopg2.connect(host=conf.db_host,port=conf.db_port,database=conf.db_name,user=conf.db_user,password=conf.db_pwd) as conn:
+    engine = create_engine('postgresql://%s:%s@%s:%s/%s' % (conf.db_user, conf.db_pwd, conf.db_host, conf.db_port, conf.db_name))
     for f in table_order:
         for fi in files:
             if f==os.path.basename(fi).split('.')[0]:
                 myprint(fi)
-                r=session.bulk_upload('masterdata',fi,'insert',f)
-                r=response_to_json(r)
-                myprint(r)
-                if len(r['errors'])!=0:
-                    sys.exit(10)
-                if r['response']['status']!='COMPLETED':
-                    if r['response']['statusDescription'].find('Duplicate Record') == -1:
-                        sys.exit(11)
+                df = pd.read_csv(fi)
+                df['cr_by'] = conf.superadmin_user
+                df['cr_dtimes'] = str(dt.utcnow())
+                df.to_sql(f,engine, index=False, if_exists='append')
 
 def args_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='directory containing all the tables in csv format')
     parser.add_argument('order', help='file that contains the table upload order')
-    parser.add_argument('--server', type=str, help='Full url to point to the server.  Setting this overrides server specified in config.py')
-    parser.add_argument('--disable_ssl_verify', help='Disable ssl cert verification while connecting to server', action='store_true')
     args = parser.parse_args()
     return args, parser
 
 def main():
-
     args, parser =  args_parse()
-
-    if args.server:
-        conf.server = args.server   # Overide
-
-    if args.disable_ssl_verify:
-        conf.ssl_verify = False
 
     files = path_to_files(args.path)
     table_order = get_order_from_list(args.order)
@@ -60,7 +51,7 @@ def main():
     init_logger('full', 'a', './out.log', level=logging.INFO)  # Append mode
     init_logger('last', 'w', './last.log', level=logging.INFO, stdout=False)  # Just record log of last run
 
-    bulk_upload_csvs_using_api(files,table_order)
+    bulk_upload_csvs_using_sql(files,table_order)
 
 if __name__=="__main__":
     main()
