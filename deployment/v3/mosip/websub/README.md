f4278477e35488a3dc3fe471f1f1fdf1b88591ac
Puneet Joshi <puneet@mosip.io>
2021-06-07 13:00:27 +0530

    [MOSIP-14462] Websub procedure added.

---
+++ deployment/v3/mosip/websub/README.md
@@ -0,0 +1,8 @@
+# Websub
+
+## Install
+* Review `values.yaml` esp. for postgres hostname
+* Install
+```
+$ ./install.sh
+``` 
---
+++ deployment/v3/mosip/websub/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Installs Websub
+NS=websub
+echo Copy configmaps
+./copy_cm.sh
+echo Copy secrets
+./copy_secrets.sh
+echo Create namespace
+kubectl create ns $NS 
+echo Istio label 
+kubectl label ns $NS istio-injection=enabled --overwrite
+helm repo update
+echo Installing Websub
+helm -n $NS install websub mosip/websub -f values.yaml
