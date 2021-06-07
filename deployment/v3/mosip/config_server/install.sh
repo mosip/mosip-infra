ec42e2be7b153df6548ee3d42f8747314e4ea7ce
Puneet Joshi <puneet@mosip.io>
2021-06-07 21:16:24 +0530

    [MOSIP-13230] Minor cleanup.

--- deployment/v3/mosip/config_server/install.sh
+++ deployment/v3/mosip/config_server/install.sh
@@ -3,12 +3,16 @@
 NS=config-server
 echo Copy configmaps
 ./copy_cm.sh
+
 echo Copy secrets
 ./copy_secrets.sh
+
 echo Create namespace
 kubectl create ns $NS
+
 echo Istio label 
 kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
+
 echo Installing config-server
 helm -n $NS install config-server mosip/config-server
