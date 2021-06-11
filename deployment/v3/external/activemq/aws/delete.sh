2cfffd223034f6a8d0b1af04a5b69e3c3b2f0a24
Puneet Joshi <puneet@mosip.io>
2021-06-11 21:47:58 +0530

    [MOSIP-12232] Activemq procedure updated.

---
+++ deployment/v3/external/activemq/aws/delete.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Uninstalls Activemq
+NS=activemq
+while true; do
+    read -p "Are you sure you want to delete ActiveMQ helm chart? (Y/n) " yn
+    if [[ $yn == "Y" ]]
+      then
+        helm -n $NS delete activemq
+        break
+      else
+        break
+    fi
+done
