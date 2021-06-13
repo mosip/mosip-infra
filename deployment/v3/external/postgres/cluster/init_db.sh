ded1ae88dd4ced57fd41f6e6fa754e10ed9b4cf4
Puneet Joshi <puneet@mosip.io>
2021-06-13 15:09:22 +0530

    [MOSIP-12620] Minor update to init db script.

--- deployment/v3/external/postgres/cluster/init_db.sh
+++ deployment/v3/external/postgres/cluster/init_db.sh
@@ -6,6 +6,8 @@ while true; do
     read -p "CAUTION: all existing data will be lost. Are you sure ? Y/n ?" yn
     if [[ $yn == "Y" ]]
       then
+        echo Removing any existing installation
+        helm -n postgres delete postgres-init
         helm -n postgres install postgres-init mosip/postgres-init -f init_values.yaml
         break
       else
