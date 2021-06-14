d0d01ae02bc19d6eef05708822cdf332c11e75ca
nitish2087 <vastav2087@gmail.com>
2021-06-14 10:33:05 +0530

    [MOSIP-12878] Folder structure - redefined

--- deployment/v3/reporting/install.sh
+++ deployment/v3/reporting/install.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
-# Installs Kafka-Debezium and Spark
-NS=reporting
 
-echo Add helm repo
-helm repo add <>
+NS=reporting
+echo Add helm repos
+helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator # for kafka
+helm repo add stable # for spark
 helm repo update
 
-echo Installing kafka
-helm -n $NS install kafka bitnami/kafka
+## Install kafka-essentials
+helm -n $NS install kafka-essential /kafka-essentials
 
-echo Installing Spark
+## Install Spark
+#install helm chart
+helm -n $NS install spark stable/spark
+
+#get pod name of spark-worker
+PODNAME=kubectl -n $NS get pods -o go-template --template ' {{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | awk '{print $1}' | grep spark-worker | tail -n 1
+
+#copy jobs to spark-worker
+kubectl -n $NS cp jobs/*.py $PODNAME:opt/spark/examples/src/main/python/
+
+
+## Run jobs
+#job-1
+kubectl -n $NS exec -it $PODNAME -- /opt/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-assembly_2.10:1.5.1 --class org.apache.spark.examples.SparkPi   --master spark://spark-master:7077  /opt/spark/examples/src/main/python/<python-job>.py 1000
+
+#job-2
+
+#job-3
 
