3d44ea926bfa93555d4e31e7e985fef86f832b63
Puneet Joshi <puneet@mosip.io>
2021-06-15 15:33:23 +0530

    [MOSIP-13460] Minor correction in install script.

--- deployment/v3/mosip/keymanager/install.sh
+++ deployment/v3/mosip/keymanager/install.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 # Installs Keymanager
 NS=keymanager
-echo Copy configmaps
-./copy_cm.sh
-
-echo Create namespace
-kubectl create ns $NS 
 
 echo Istio label 
 kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
+
+echo Copy configmaps
+./copy_cm.sh
 
 echo Installing keymanager
 helm -n $NS install keymanager mosip/keymanager
