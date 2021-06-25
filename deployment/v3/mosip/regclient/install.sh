9f939b8df00e21efccbcdfc8a194dd647d21ae7b
Puneet Joshi <puneet@mosip.io>
2021-06-25 11:51:36 +0530

    [MOSIP-15110] Minor update to regclient install procedure.

--- deployment/v3/mosip/regclient/install.sh
+++ deployment/v3/mosip/regclient/install.sh
@@ -12,18 +12,16 @@ helm repo update
 echo Copy configmaps
 ./copy_cm.sh
 
-REGCLIENT_URL=`kubectl get cm global -o json | jq .data.\"mosip-regclient-url\" | tr -d '"'`
 REGCLIENT_HOST=`kubectl get cm global -o json | jq .data.\"mosip-regclient-host\" | tr -d '"'`
-INTERNAL_URL=`kubectl get cm global -o json | jq .data.\"mosip-api-internal-url\" | tr -d '"'`
-HEALTH_URL=$INTERNAL_URL/v1/syncdata/actuator/health
 INTERNAL_HOST=`kubectl get cm global -o json | jq .data.\"mosip-api-internal-host\" | tr -d '"'`
+HEALTH_URL=https://$INTERNAL_HOST/v1/syncdata/actuator/health
 
 echo Install reg client downloader. This may take a few minutes ..
 helm -n $NS install regclient mosip/regclient \
-  --set regclient.upgradeServerUrl=$REGCLIENT_URL \
+  --set regclient.upgradeServerUrl=https://$REGCLIENT_HOST \
   --set regclient.healthCheckUrl=$HEALTH_URL \
   --set istio.host=$REGCLIENT_HOST \
   --wait
 
 echo Get your download url from here
-echo $REGCLIENT_URL/
+echo https://$REGCLIENT_HOST/
