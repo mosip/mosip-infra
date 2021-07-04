e2e7f8f51519a518e9d46451d145ea14707e4de3
Puneet Joshi <puneet@mosip.io>
2021-07-04 06:56:56 +0530

    [MOSIP-15491] Machine type converted to xlsx.

--- deployment/v3/utils/onboard/machine/lib/add_type.py
+++ deployment/v3/utils/onboard/machine/lib/add_type.py
@@ -4,6 +4,7 @@ import sys
 import traceback
 import argparse
 import csv
+import pandas as pd
 from api import *
 sys.path.insert(0, '../')
 from utils import *
@@ -11,13 +12,9 @@ from utils import *
 def args_parse(): 
    parser = argparse.ArgumentParser()
    parser.add_argument('server', type=str, help='Full url to point to the server')
-   parser.add_argument('code', type=str, help='Machine type code')
-   parser.add_argument('name', type=str, help='Machine type name')
-   parser.add_argument('description', type=str, help='Machine type description in "quotes"')
-   parser.add_argument('language', type=str, help='Language code. You need to add for all languages. The type will not be active till all languages are added.')
+   parser.add_argument('xlsx', type=str, help='Excel container machine specification data')
    parser.add_argument('admin', type=str, help='Keycloak user with GLOBAL_ADMIN role who is adding user')
    parser.add_argument('admin_pwd', type=str, help='Password for admin')
-   parser.add_argument('--update', help='Update info in DB. By default info will not be updated', action='store_true')
    parser.add_argument('--disable_ssl_verify', help='Disable ssl cert verification while connecting to server', action='store_true')
    args = parser.parse_args()
    return args
@@ -29,16 +26,16 @@ def main():
     if args.disable_ssl_verify:
         ssl_verify = False
 
-    update = False
-    if args.update:
-        update = True
-
     init_logger('full', 'a', './out.log', level=logging.INFO)  # Append mode
+
+    df = pd.read_excel(args.xlsx)
 
     try:
         session = MosipSession(args.server, args.admin, args.admin_pwd, 'regproc', ssl_verify)
-        r = session.add_machine_type(args.code, args.name, args.description, args.language, update=update)
-        myprint(r)
+        for i,row in df.iterrows():
+            r = session.add_type(df['code'][i], df['name'][i], df['description'][i], df['language'][i], 
+                                 update=True)
+            myprint(r)
     except:
         formatted_lines = traceback.format_exc()
         myprint(formatted_lines)
--- deployment/v3/utils/onboard/machine/mtype.sh
+++ deployment/v3/utils/onboard/machine/mtype.sh
@@ -2,5 +2,4 @@
 # Example script 
 
 SERVER=https://api-internal.sandbox.xyz.net
-python3 lib/add_machine_type.py $SERVER LP1 Laptop-1 "Nice looking laptop" eng admin <password>
-python3 lib/add_machine_type.py $SERVER LP1 Laptop-1 "Joli ordinateur portable" fra admin <password>
+python3 lib/add_machine_type.py $SERVER xlxs/type.xlsx admin <password>
