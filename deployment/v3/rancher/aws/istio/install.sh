6628c40e0059d47fa09abc19f343a9863a66c44f
Lalith Kota <kotalalith@gmail.com>
2021-07-30 18:44:42 +0530

    Changes to install.sh scripts & values.yaml for Rancher-IAM cluster

--- deployment/v3/rancher/aws/istio/install.sh
+++ deployment/v3/rancher/aws/istio/install.sh
@@ -13,7 +13,8 @@ kubectl -n istio-system rollout status deploy istio-ingressgateway
 
 echo Installing gateways, proxy protocol
 HOST=rancher.xyz.net
-helm -n istio-system install istio-addons chart/istio-addons --set host=HOST
+kubectl create ns cattle-system
+helm -n cattle-system install istio-addons chart/istio-addons --set host=$HOST
 
 echo ------ IMPORTANT ---------
 echo If you already have pods running with envoy sidecars, restart all of them NOW.  Check if all of them appear with command "istioctl proxy-status"
