ef558c5e2d9ccfa9fea1caf4a7044db0209f3ee7
Puneet Joshi <puneet@mosip.io>
2021-08-01 12:57:03 +0530

    [MOSIP-14287] Restart script added.

---
+++ deployment/v3/mosip/ida/restart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Restart the deployment
+# CAUTION: Restart will restart softhms as well.  Although, that is not expected to create any problems.
+NS=ida
+kubectl -n $NS rollout restart deploy
