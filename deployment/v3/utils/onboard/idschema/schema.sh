d09f80ebfa679212e602adb9bb960c0a05964c51
Puneet Joshi <puneet@mosip.io>
2021-08-08 06:19:14 +0530

    [MOSIP-11894] Minor doc update.

--- deployment/v3/utils/onboard/idschema/schema.sh
+++ deployment/v3/utils/onboard/idschema/schema.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Example script to update schema
 SERVER=https://api-internal.sandbox.xyz.net
-PASS=<admin password>
+# Admin with GLOBAL_ADMIN role
+PASS=<admin password>  
 python3 lib/update_idschema.py $SERVER NewSchema "Schema for my country" schemas/ui_spec.json admin $PASS
