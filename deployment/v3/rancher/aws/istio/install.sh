973bf2509f5afd8fca8a321e3cf3c517682f2d62
Puneet Joshi <puneet@mosip.io>
2021-08-18 18:02:33 +0530

    [MOSIP-11894] Rancher install scripts updated.

--- deployment/v3/rancher/aws/istio/install.sh
+++ deployment/v3/rancher/aws/istio/install.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 # Install ingress gateway
+KC="--kubeconfig $HOME/.kube/iam_config"
+
 echo Operator init
-istioctl operator init
+istioctl $KC  operator init
 
 echo Create ingress gateways and load balancers
-kubectl apply -f iop.yaml
+kubectl $KC apply -f iop.yaml
 
 echo Wait for all resources to come up
-sleep 10 
-kubectl -n istio-system rollout status deploy istiod
-kubectl -n istio-system rollout status deploy istio-ingressgateway
+sleep 20 
+kubectl $KC -n istio-system rollout status deploy istiod
+kubectl $KC -n istio-system rollout status deploy istio-ingressgateway
 
 echo Installing gateways, proxy protocol
 HOST=rancher.xyz.net
-kubectl create ns cattle-system
-helm -n cattle-system install istio-addons chart/istio-addons --set host=$HOST
+kubectl $KC create ns cattle-system
+helm $KC -n cattle-system install istio-addons chart/istio-addons --set host=$HOST
 
 echo ------ IMPORTANT ---------
 echo If you already have pods running with envoy sidecars, restart all of them NOW.  Check if all of them appear with command "istioctl proxy-status"
