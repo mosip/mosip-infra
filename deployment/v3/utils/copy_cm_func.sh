e99ba8bc77ef424498e268da941beaaed7ff1f5b
Puneet Joshi <puneet@mosip.io>
2021-08-19 10:12:27 +0530

    Fixes in cm copy function

--- deployment/v3/utils/copy_cm_func.sh
+++ deployment/v3/utils/copy_cm_func.sh
@@ -16,14 +16,14 @@ else
   exit 1
 fi
 
-if [ ! -z $5 ]
+if [ $# -ge 5 ]
 then
   KC_SOURCE="kubectl --kubeconfig $5"
 else
   KC_SOURCE=kubectl
 fi
-$KC_SOURCE -n $4 delete --ignore-not-found=true $RESOURCE $2
-$KC_SOURCE -n $3 get $RESOURCE $2 -o yaml | sed "s/namespace: $3/namespace: $4/g" | $KC_SOURCE -n $4 create -f -  
+kubectl -n $4 delete --ignore-not-found=true $RESOURCE $2
+$KC_SOURCE -n $3 get $RESOURCE $2 -o yaml | sed "s/namespace: $3/namespace: $4/g" | kubectl -n $4 create -f -  
 
 
 
