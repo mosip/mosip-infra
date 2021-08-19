e71f818f86c851c450c9975069d63d05af5d2b66
Puneet Joshi <puneet@mosip.io>
2021-08-19 10:06:15 +0530

    [MOSIP-16645] Updated for 1.2.0.

--- deployment/v3/external/postgres/cluster/init_db.sh
+++ deployment/v3/external/postgres/cluster/init_db.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # Script to initialize the DB. 
 NS=postgres
+CHART_VERSION=1.2.0
 helm repo update
 while true; do
-    read -p "CAUTION: all existing data will be lost. Are you sure ? Y/n ?" yn
+    read -p "CAUTION: all existing data will be lost. Are you sure?(Y/n)" yn
     if [[ $yn == "Y" ]]
       then
         echo Removing any existing installation
         helm -n $NS delete postgres-init
-        helm -n $NS install postgres-init mosip/postgres-init -f init_values.yaml --wait --wait-for-jobs
+        echo Initializing DB
+        helm -n $NS install postgres-init mosip/postgres-init -f init_values.yaml --version $CHART_VERSION --wait --wait-for-jobs
         break
       else
         break
