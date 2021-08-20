c7b8b60f9a548adacc58f54d3de3fd999251e871
Puneet Joshi <puneet@mosip.io>
2021-08-20 15:42:54 +0530

    [MOSIP-16645] Updated for 1.2.0.

--- deployment/v3/mosip/pms/install.sh
+++ deployment/v3/mosip/pms/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Installs all PMS charts
 NS=pms
-CHART_VERSION=1.1.5
+CHART_VERSION=1.2.0
 
 echo Create namespace
 kubectl create ns $NS
@@ -14,7 +14,7 @@ echo Copy configmaps
 ./copy_cm.sh
 
 echo Installing partner manager
-helm -n $NS install pms-partner mosip/pms-partner -f values.yaml --version $CHART_VERSION
+helm -n $NS install pms-partner mosip/pms-partner --version $CHART_VERSION
 
 echo Installing policy manager
 helm -n $NS install pms-policy mosip/pms-policy --version $CHART_VERSION
