8df9e70e8b6e2fdaf482202b08f8ecdf350fd5dd
Lalith Kota <kotalalith@gmail.com>
2021-09-03 12:24:12 +0530

    [MOSIP-12878] Added Timestamp Changes, and kafka changed to optional

--- deployment/v3/reporting/run_connect_api.sh
+++ deployment/v3/reporting/run_connect_api.sh
@@ -4,4 +4,4 @@ CLIENT_POD_NAME=kafka-client
 NS=reporting
 API_CALL=$(cat $1)
 
-kubectl exec -it $CLIENT_POD_NAME -n $NS -- sh -c "$API_CALL" > /dev/null
+kubectl exec -it $CLIENT_POD_NAME -n $NS -- sh -xc "$API_CALL"
