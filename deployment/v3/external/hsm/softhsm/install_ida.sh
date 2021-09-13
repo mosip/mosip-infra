be1c86a95ad3f343ef3aaa153e4ffc6464fde2e9
Puneet Joshi <puneet@mosip.io>
2021-09-13 11:35:32 +0530

    [MOSIP-16645] Chart version added.

--- deployment/v3/external/hsm/softhsm/install_ida.sh
+++ deployment/v3/external/hsm/softhsm/install_ida.sh
@@ -2,6 +2,7 @@
 # Installs Softhsm for IDA
 NS=ida
 STORAGE_CLASS=gp2
+CHART_VERSION=1.2.0
 
 echo Create namespaces
 kubectl create ns $NS 
@@ -11,4 +12,4 @@ kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
 
 echo Installing Softhsm for IDA 
-helm -n $NS install softhsm-ida mosip/softhsm --set fullnameOverride=softhsm-ida --set persistence.storageClass=$STORAGE_CLASS -f values.yaml
+helm -n $NS install softhsm-ida mosip/softhsm --set fullnameOverride=softhsm-ida --set persistence.storageClass=$STORAGE_CLASS -f values.yaml --version $CHART_VERSION
