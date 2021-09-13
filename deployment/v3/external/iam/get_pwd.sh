4ee0d5a0c957a4f1dbbf29d32ef6225698577eaf
Puneet Joshi <puneet@mosip.io>
2021-09-13 22:40:58 +0530

    [MOSIP-16645] get_pwd.sh script enhanced.

--- deployment/v3/external/iam/get_pwd.sh
+++ deployment/v3/external/iam/get_pwd.sh
@@ -2,10 +2,20 @@
 # Get admin password
 alias KK='kubectl --kubeconfig $HOME/.kube/iam_config'
 echo Keycloak admin password: $(KK get secret --namespace keycloak keycloak -o jsonpath="{.data.admin-password}" | base64 --decode)
-echo Keycloak prereg client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.preregistration_mosip_prereg_client_secret}" | base64 --decode)
-echo Keycloak admin client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_admin_client_secret}" | base64 --decode)
+echo mosip-prereg-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.preregistration_mosip_prereg_client_secret}" | base64 --decode)
+
+echo mosip-admin-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_admin_client_secret}" | base64 --decode)
+
 echo Keycloak postgresql password: $(KK get secret --namespace keycloak keycloak-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+
 echo Keycloak postgresql postgres password: $(KK get secret --namespace keycloak keycloak-postgresql -o jsonpath="{.data.postgresql-postgres-password}" | base64 --decode)
-echo Keycloak pms client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_pms_client_secret}" | base64 --decode)
-echo Keycloak regproc client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_regproc_client_secret}" | base64 --decode)
-echo Keycloak ida client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_ida_client_secret}" | base64 --decode)
+
+echo mosip-pms-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_pms_client_secret}" | base64 --decode)
+
+echo mosip-partner-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_partner_client_secret}" | base64 --decode)
+
+echo mosip-partnermanager-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_partnermanager_client_secret}" | base64 --decode)
+
+echo mosip-regproc-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_regproc_client_secret}" | base64 --decode)
+
+echo mosip-ida-client password: $(KK get secret --namespace keycloak keycloak-client-secrets -o jsonpath="{.data.mosip_mosip_ida_client_secret}" | base64 --decode)
