61d89053fdc2016f978f64356850c156c1566c50
Puneet Joshi <puneet@mosip.io>
2021-09-17 15:13:05 +0530

    Minor cleanup.

--- deployment/v3/mosip/config_server/copy_cm.sh
+++ deployment/v3/mosip/config_server/copy_cm.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # Copy configmaps from other namespaces
 # DST_NS: Destination namespace 
-# Change OBJECT_STORE to "minio" if you are using the same for object store
 COPY_UTIL=../../utils/copy_cm_func.sh
 DST_NS=config-server
-OBJECT_STORE=s3
 $COPY_UTIL configmap global default $DST_NS 
 $COPY_UTIL configmap keycloak-host keycloak $DST_NS ~/.kube/iam_config
 $COPY_UTIL configmap activemq-activemq-artemis-share activemq $DST_NS
