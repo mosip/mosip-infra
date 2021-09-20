6fb9a06914b1d1b3487f90019cee60f40940f14a
Puneet Joshi <puneet@mosip.io>
2021-09-20 15:40:26 +0530

    [MOSIP-17626] Script update.

--- deployment/v3/mosip/kernel/masterdata/upload/upload_md.sh
+++ deployment/v3/mosip/kernel/masterdata/upload/upload_md.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-# 
+# Usage: ./upload_md.sh [kubeconfig file] 
+# Default kubeconfig file is $HOME/.kube/config
+if [ $# -ge 1 ]
+  then
+    export KUBECONFIG=$1
+fi
 read -p "Enter IAM username: " iam_user
 
 # This username is hardcoded in sql scripts
 DB_PWD=$(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
 DB_HOST=`kubectl get cm global -o json | jq .data.\"mosip-api-internal-host\" | tr -d '"'`
 DB_PORT=5432
-XLS=xlsx2
+XLS=xlsx
 
 while true; do
     read -p "WARNING: All existing masterdata will be erased. Are you sure?(Y/n) " yn
