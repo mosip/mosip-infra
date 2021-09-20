0aba4d4191cc7bb2d8d5132df750e263e9ea337b
Puneet Joshi <puneet@mosip.io>
2021-09-20 15:09:49 +0530

    [MOSIP-17626] Enhancement for multiple kubeconfig files.

--- deployment/v3/external/postgres/cluster/get_pwd.sh
+++ deployment/v3/external/postgres/cluster/get_pwd.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # Get postgres password
+# Usage: sh get_pwd.sh [kubeconfig file] 
+# Default kubeconfig file is $HOME/.kube/config
+if [ $# -ge 1 ]
+  then
+    export KUBECONFIG=$1
+fi
+
 echo postgres: $(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
 echo db-user: $(kubectl get secret --namespace postgres db-common-secrets -o jsonpath="{.data.db-dbuser-password}" | base64 --decode)
