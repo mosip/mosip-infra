05180075a4f7e0fdb2af844565a4b7981dfaa67b
Puneet Joshi <puneet@mosip.io>
2021-09-28 17:44:44 +0530

    [MOSIP-12878] Added echo statements.

--- deployment/v3/reporting/install.sh
+++ deployment/v3/reporting/install.sh
@@ -10,23 +10,26 @@ NS=reporting
 CHART_VERSION=1.2.0
 
 # Add helm repos
+echo "Adding helm repos"
 helm repo add mosip https://mosip.github.io/mosip-helm
 helm repo update
 
 # Creating namespace with istio-injeciton
+echo "Creating namespace"
 kubectl create ns $NS
 kubectl label ns $NS istio-injection=enabled
 
 ## Configure Postgres essentials
 # Assumed Postgres is installed in 'postgres' namespace with extended.conf as extended config.
 
-## Copy db secrets from postgres namespace to current namespace
+echo "Copying secrets"
 ./copy_secret.sh $NS
 
-## Install reporting helm chart
+echo "Installing reporting helm"
 helm -n $NS install reporting mosip/reporting -f values.yaml --wait --version $CHART_VERSION
 
+echo "Waiting for helm chart to install"
 sleep 30s
 
-## Install reporting-init helm chart
+echo "Installing reporting-init helm"
 helm -n $NS install reporting-init mosip/reporting-init -f values-init.yaml --wait --version $CHART_VERSION
