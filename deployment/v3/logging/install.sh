63aab1b2d2600ca79eb947f204d8a2d9a8886ddf
Lalith Kota <kotalalith@gmail.com>
2021-09-29 17:38:48 +0530

    [MOSIP-17679] Changed kibana to install with elasticsearch

--- deployment/v3/logging/install.sh
+++ deployment/v3/logging/install.sh
@@ -5,13 +5,15 @@ NS=cattle-logging-system
 echo Create namespace logging
 kubectl create namespace $NS
 
-echo Installing Bitnami Elasticsearch
+echo Updating helm repos
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+echo Installing Bitnami Elasticsearch and Kibana
 helm -n $NS install elasticsearch bitnami/elasticsearch -f es_values.yaml --wait
 
-KIBANA_HOST=`kubectl get cm global -o json | jq .data.\"mosip-kibana-host\" | tr -d '"'`
-KIBANA_NAME=mykibana
-echo Installing Kibana
-helm -n $NS install $KIBANA_NAME bitnami/kibana -f kibana_values.yaml --wait
+KIBANA_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-kibana-host})
+KIBANA_NAME=kibana
 
 echo Install istio addons
 helm -n $NS install istio-addons chart/istio-addons --set kibanaHost=$KIBANA_HOST --set installName=$KIBANA_NAME
