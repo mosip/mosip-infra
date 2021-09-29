b1f3057d6c1314fb85d87addb3eeb7b81ff07292
Lalith Kota <kotalalith@gmail.com>
2021-09-29 17:36:30 +0530

    [MOSIP-17618] Added installation of kafka-ui

--- deployment/v3/external/kafka/install.sh
+++ deployment/v3/external/kafka/install.sh
@@ -7,7 +7,20 @@ kubectl create ns $NS
 
 echo Istio label 
 kubectl label ns $NS istio-injection=enabled --overwrite
+
+echo Updating helm repos
+helm repo add kafka-ui https://provectus.github.io/kafka-ui
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 echo Installing kafka
-helm -n kafka install kafka bitnami/kafka -f values.yaml
+helm -n $NS install kafka bitnami/kafka -f values.yaml --wait
+
+echo Installing kafka-ui
+helm -n $NS install kafka-ui kafka-ui/kafka-ui -f ui-values.yaml --wait
+
+KAFKA_UI_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-kafka-host})
+KAFKA_UI_NAME=kafka-ui
+
+echo Install istio addons
+helm -n $NS install istio-addons chart/istio-addons --set kafkaUiHost=$KAFKA_UI_HOST --set installName=$KAFKA_UI_NAME
