6fd6b623ba5315ea23f727518331aa90680efa62
Puneet Joshi <puneet@mosip.io>
2021-09-30 19:04:23 +0530

    [MOSIP-17836] Updated for 1.2.0-rc2

--- build/postgres-init/docker_build.sh
+++ build/postgres-init/docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # ./docker_build.sh git_tag
 # git_tag for all repos corresponding to postgres-init:1.2.0-rc1 is mosip-16601
+# git_tag for all repos corresponding to postgres-init:1.2.0-rc2 is mosip-17836
 
 mkdir -p repos
 cd repos
@@ -12,4 +13,4 @@ git clone -b $1 https://github.com/mosip/id-authentication
 git clone -b $1 https://github.com/mosip/id-repository
 git clone -b $1 https://github.com/mosip/admin-services
 cd ../
-docker build -t mosipdev/postgres-init:1.2.0-rc1
+docker build -t mosipdev/postgres-init:1.2.0-rc2
