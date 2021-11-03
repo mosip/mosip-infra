961a736b8a471ed3db48d535b5756d2844286513
Puneet Joshi <puneet@mosip.io>
2021-11-03 21:45:56 +0530

    [MOSIP-18373] Admin portal used for onboarding machine, users.  Device onboarding removed as not used anymore.

--- deployment/v3/utils/onboard/idschema/schema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Example script to update schema
-SERVER=https://api-internal.sandbox.xyz.net
-# Admin with GLOBAL_ADMIN role
-PASS=<admin password>  
-python3 lib/update_idschema.py $SERVER NewSchema "Schema for my country" schemas/ui_spec.json admin $PASS
