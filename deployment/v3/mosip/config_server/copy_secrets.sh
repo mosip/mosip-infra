f2762e5b9463bc420a3c13278cd6f614fda4d222
Puneet Joshi <puneet@mosip.io>
2021-12-09 13:01:43 +0530

    Changes for in-cluster Keycloak install.

--- deployment/v3/mosip/config_server/copy_secrets.sh
+++ deployment/v3/mosip/config_server/copy_secrets.sh
@@ -4,8 +4,8 @@
 COPY_UTIL=../../utils/copy_cm_func.sh
 DST_NS=config-server
 $COPY_UTIL secret db-common-secrets postgres $DST_NS 
-$COPY_UTIL secret keycloak keycloak $DST_NS ~/.kube/iam_config
-$COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS ~/.kube/iam_config
+$COPY_UTIL secret keycloak keycloak $DST_NS 
+$COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
 $COPY_UTIL secret softhsm-kernel keymanager $DST_NS
 $COPY_UTIL secret activemq-activemq-artemis activemq $DST_NS 
 $COPY_UTIL secret softhsm-ida ida $DST_NS 
