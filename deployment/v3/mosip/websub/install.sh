d8ce52a668fbbaadc807635d576b3fbfc2028cfc
Puneet Joshi <puneet@mosip.io>
2022-01-07 11:43:25 +0530

    Websub install sequence updated.

--- deployment/v3/mosip/websub/install.sh
+++ deployment/v3/mosip/websub/install.sh
@@ -14,5 +14,5 @@ echo Copy configmaps
 ./copy_cm.sh
 
 echo Installing Websub
+helm -n $NS install websub-consolidator mosip/websub-consolidator --version $CHART_VERSION --wait
 helm -n $NS install websub mosip/websub --version $CHART_VERSION
-helm -n $NS install websub-consolidator mosip/websub-consolidator --version $CHART_VERSION
