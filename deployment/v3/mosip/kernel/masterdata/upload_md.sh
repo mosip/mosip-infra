819496075b22944b5f29bb749f84c62c59e2609b
Puneet Joshi <puneet@mosip.io>
2022-01-09 09:57:04 +0530

    [MOSIP-13679] Argument added for xls path.

--- deployment/v3/mosip/kernel/masterdata/upload_md.sh
+++ deployment/v3/mosip/kernel/masterdata/upload_md.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# Usage: ./upload_md.sh [kubeconfig file]
+# Usage: ./upload_md.sh <mosip-data repo path> [kubeconfig file]
 # Default kubeconfig file is $HOME/.kube/config
-if [ $# -ge 1 ]
+if [ $# -ge 2 ]
   then
-    export KUBECONFIG=$1
+    export KUBECONFIG=$2
 fi
 
 read -p "Enter IAM username: " iam_user
@@ -12,8 +12,7 @@ read -p "Enter IAM username: " iam_user
 DB_PWD=$(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath={.data.postgresql-password} | base64 --decode)
 DB_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host})
 DB_PORT=5432
-MOSIP_DATA_REPO_BASE=/Users/puneet/Documents/mosip/lts/mosip-data/
-XLS_FOLDER_PATH=$MOSIP_DATA_REPO_BASE/data-dml/mosip_master/xlsx
+XLS_FOLDER_PATH=$1/data-dml/mosip_master/xlsx
 
 while true; do
     read -p "WARNING: All existing masterdata will be erased. Are you sure?(Y/n) " yn
