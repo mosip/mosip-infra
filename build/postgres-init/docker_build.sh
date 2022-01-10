4e87ac79668005f3cbb49f4eeaf164e523f12a47
Puneet Joshi <puneet@mosip.io>
2022-01-10 18:00:25 +0530

    Cleanup.

--- build/postgres-init/docker_build.sh
+++ build/postgres-init/docker_build.sh
@@ -3,16 +3,6 @@
 # git_tag for all repos corresponding to postgres-init:1.2.0-rc1 is mosip-16601
 # git_tag for all repos corresponding to postgres-init:1.2.0-rc2 is mosip-17836
 
-#mkdir -p repos
-#cd repos
-#git clone -b $1 https://github.com/mosip/commons 
-#git clone -b $1 https://github.com/mosip/pre-registration
-#git clone -b $1 https://github.com/mosip/registration
-#git clone -b $1 https://github.com/mosip/partner-management-services
-#git clone -b $1 https://github.com/mosip/id-authentication
-#git clone -b $1 https://github.com/mosip/id-repository
-#git clone -b $1 https://github.com/mosip/admin-services
-#cd ../
 TAG=mosipdev/postgres-init:1.2.0-rc2
 echo Building $TAG
 docker build -t $TAG .
