78f577809a4493baba796ae3731ae00aff5ae1ce
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2022-02-08 21:33:29 +0530

    created single shell script

--- build/file-server/get-cert.sh
+++ build/file-server/get-cert.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
-bash auth.sh
+#echo "* Request for authorization"
+curl -s -D - -o /dev/null -X "POST" \
+  "$host_url_env/v1/authmanager/authenticate/clientidsecretkey" \
+  -H "accept: */*" \
+  -H "Content-Type: application/json" \
+  -d '{
+  "id": "string",
+  "version": "string",
+  "requesttime": "'$date_env'",
+  "metadata": {},
+  "request": {
+    "clientId": "mosip-regproc-client",
+    "secretKey": "'$clientsecret_env'",
+    "appId": "regproc"
+  }
+}' > temp.txt 2>&1 &
+
+sleep 10
+
 TOKEN=$(cat -n temp.txt | sed -n '/authorization:/,/\;.*/p' |  sed 's/.*authorization://; s/$\n.*//' | awk 'NR==1{print $1}')
-RESULT=`curl -v \
+
+curl -X "GET" \
+  -H "Accept: application/json" \
   --cookie "Authorization=$TOKEN" \
-  "$host_url_env/v1/keymanager/getCertificate?applicationId=KERNEL&referenceId=SIGN" \
-  -H "accept: */*"`
+  "$host_url_env/v1/keymanager/getCertificate?applicationId=KERNEL&referenceId=SIGN" > result.txt
+
+RESULT=$(cat result.txt)
 CERT=$(echo $RESULT | sed 's/.*certificate\":\"//g' | sed 's/\".*//g')
 echo $CERT | sed -e 's/\\n/\n/g' > cert.pem
 openssl x509 -pubkey -noout -in cert.pem  > pubkey.pem
