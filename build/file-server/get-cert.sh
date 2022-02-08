8e4744c849ef489c51024e18ec783ac27b8b0788
Mandeep Dhiman <46880392+mandeepdhiman123@users.noreply.github.com>
2022-02-08 18:42:17 +0530

    Update get-cert.sh

--- build/file-server/get-cert.sh
+++ build/file-server/get-cert.sh
@@ -10,5 +10,7 @@ RESULT=`curl -X "GET" \
 
 CERT=$(echo $RESULT | sed 's/.*certificate\":\"//g' | sed 's/\".*//g')
 echo $CERT | sed -e 's/\\n/\n/g' > cert.pem
+cat cert.pem
 openssl x509 -pubkey -noout -in cert.pem  > pubkey.pem
+cat pubkey.pem
 sed -i "s&replace-public-key&$(cat pubkey.pem | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\\\n/g')&g" ./public-key.json
