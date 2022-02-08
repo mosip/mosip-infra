d4f52403257979d47c11b56110fe6cf3550a3fff
Mandeep Dhiman <46880392+mandeepdhiman123@users.noreply.github.com>
2022-02-08 21:01:18 +0530

    Update get-cert.sh

--- build/file-server/get-cert.sh
+++ build/file-server/get-cert.sh
@@ -2,14 +2,11 @@
 
 bash auth.sh
 TOKEN=$(cat -n temp.txt | sed -n '/authorization:/,/\;.*/p' |  sed 's/.*authorization://; s/$\n.*//' | awk 'NR==1{print $1}')
-RESULT=`curl -X "GET" \
+RESULT=`curl --http2 -X "GET" \
   --cookie "Authorization=$TOKEN" \
   "$host_url_env/v1/keymanager/getCertificate?applicationId=KERNEL&referenceId=SIGN" \
   -H "accept: */*"`
-echo $RESULT
 CERT=$(echo $RESULT | sed 's/.*certificate\":\"//g' | sed 's/\".*//g')
 echo $CERT | sed -e 's/\\n/\n/g' > cert.pem
-cat cert.pem
 openssl x509 -pubkey -noout -in cert.pem  > pubkey.pem
-cat pubkey.pem
 sed -i "s&replace-public-key&$(cat pubkey.pem | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\\\n/g')&g" ./public-key.json
