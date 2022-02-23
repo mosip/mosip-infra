f756caaf785f255f731c68d7fdff1c70563d31bd
Puneet Joshi <puneet@mosip.io>
2022-02-23 15:37:16 +0530

    [MOSIP-20285] New helm chart used.

--- deployment/v3/external/antivirus/clamav/README.md
+++ deployment/v3/external/antivirus/clamav/README.md
@@ -3,6 +3,8 @@
 ClamAV is open source antivirus software. We provide a helm chart to install ClamAV. The same may be used for development and testing; for production deployments you may need to use a production-grade antivirus software.
 
 ## Install
+* Update `replicaCount` and docker image (if required).
+* Run
 ```sh
 ./install.sh
 ```
--- deployment/v3/external/antivirus/clamav/install.sh
+++ deployment/v3/external/antivirus/clamav/install.sh
@@ -7,14 +7,14 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=clamav
-CHART_VERSION=1.2.0
 
 echo Create namespace
 kubectl create ns $NS 
 
 echo Istio label 
 kubectl label ns $NS istio-injection=enabled --overwrite
+helm repo add wiremind https://wiremind.github.io/wiremind-helm-charts
 helm repo update
 
 echo Installing Clamav
-helm -n $NS install clamav mosip/clamav --set replicaCount=1 --version $CHART_VERSION
+helm -n $NS install clamav wiremind/clamav -f values.yaml
