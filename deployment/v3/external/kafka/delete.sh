4f40b688f4b5dbc4588ee89f47fb7ac813793737
ckm007 <chandrakeshavmishra@gmail.com>
2022-02-26 16:54:53 +0000

    [MOSIP-20331] updated kafka install scripts

--- deployment/v3/external/kafka/delete.sh
+++ deployment/v3/external/kafka/delete.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # Uninstalls kafka
+## Usage: ./delete.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
 NS=kafka
 while true; do
     read -p "Are you sure you want to delete kafka helm chart? Y/n ?" yn
-    if [ $yn == "Y" ]
+    if [ $yn = "Y" ]
       then
         helm -n $NS delete kafka
+        helm -n $NS delete kafka-ui
         break
       else
         break
--- deployment/v3/external/kafka/restart.sh
+++ deployment/v3/external/kafka/restart.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 # Restart the deployment
+## Usage: ./restart.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
 NS=kafka
 kubectl -n $NS rollout restart statefulset
