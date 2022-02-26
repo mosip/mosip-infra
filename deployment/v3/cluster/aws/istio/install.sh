f1cf831336d68dc6970648d18c245983c444ed65
ckm007 <chandrakeshavmishra@gmail.com>
2022-02-26 16:10:29 +0000

    [MOSIP-20331] updated cluster creation part

--- deployment/v3/cluster/aws/istio/install.sh
+++ deployment/v3/cluster/aws/istio/install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # Install ingress gateways
+## Usage: ./install.sh [kubeconfig]
 
-#NOTE: istioctl is specific to kubeconfig file. If you've more than one config files, please specify them like mentioned below:
-#istioctl --kubeconfig <path-to-config-file> or use -c as shorthand for --kubeconfig.
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
 echo Operator init
 istioctl operator init
 
@@ -16,8 +19,8 @@ kubectl -n istio-system rollout status deploy istio-ingressgateway
 kubectl -n istio-system rollout status deploy istio-ingressgateway-internal
 
 echo Installing gateways, proxy protocol, authpolicies
-PUBLIC=`kubectl get cm global -o json | jq .data.\"mosip-api-host\"`
-INTERNAL=`kubectl get cm global -o json | jq .data.\"mosip-api-internal-host\"`
+PUBLIC=$(kubectl get cm global -o jsonpath={.data.mosip-api-host})
+INTERNAL=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host})
 echo Public domain: $PUBLIC
 echo Internal domain: $INTERNAL
 helm -n istio-system install istio-addons chart/istio-addons --set publicHost=$PUBLIC --set internalHost=$INTERNAL
