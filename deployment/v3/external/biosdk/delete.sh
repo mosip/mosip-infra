c8501ec47bf6c9f6658d27ceedecfb14b276c6df
ckm007 <chandrakeshavmishra@gmail.com>
2022-02-26 16:34:54 +0000

    [MOSIP-20331] updated biosdk install scripts

--- deployment/v3/external/biosdk/delete.sh
+++ deployment/v3/external/biosdk/delete.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # Uninstalls Biosdk
+## Usage: ./delete.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+
 NS=biosdk
 while true; do
-    read -p "Are you sure you want to delete Biosdk Service helm chart?(Y/n) " yn
-    if [ $yn == "Y" ]
+    read -p "Are you sure you want to delete mock-biosdk helm chart? Y/n ?" yn
+    if [ $yn = "Y" ]
       then
         helm -n $NS delete biosdk-service
         break
