726b85d0cb5a1bcbc542113f469b423e3d4e7373
Puneet Joshi <puneet@mosip.io>
2022-03-01 17:50:39 +0530

    [MOSIP-17992] Cleanup.

--- deployment/v3/utils/onboard/archive/preinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SUDO=
-$SUDO pip3 install jinja2
-$SUDO pip3 install pyyaml
-$SUDO yum install -y policycoreutils-python
-$SUDO pip3 install requests
-$SUDO pip3 install psycopg2-binary
-$SUDO pip3 install cryptography
