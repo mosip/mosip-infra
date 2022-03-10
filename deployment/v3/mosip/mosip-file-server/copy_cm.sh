61fa80a6ddfb061d6aaa9fa8389201569181ce33
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-03-10 18:59:09 +0530

    [ MOSIP-19943 ] updated mosip-file-server scripts

---
+++ deployment/v3/mosip/mosip-file-server/copy_cm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Copy configmaps from other namespaces
+# DST_NS: Destination namespace
+
+COPY_UTIL=../../utils/copy_cm_func.sh
+DST_NS=mosip-file-server
+
+$COPY_UTIL configmap config-server-share config-server $DST_NS
