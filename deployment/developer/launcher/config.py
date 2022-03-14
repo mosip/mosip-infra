6391d25015ce48503ce146b3803af4444728053d
Francis Okumu <60089305+francisokumu@users.noreply.github.com>
2022-03-14 13:25:36 +0300

    Changed MOSIP Version

--- deployment/developer/launcher/config.py
+++ deployment/developer/launcher/config.py
@@ -15,7 +15,7 @@
 import os
 
 MOSIP_DIR = os.path.join(os.environ['HOME'], 'mosip')
-MOSIP_VERSION = '0.9.1'  # Such a tag should exist on the repo
+MOSIP_VERSION = '1.1.5.5'  # Such a tag should exist on the repo
 MOSIP_REPO = 'https://github.com/mosip/commons'
 
 SOFTHSM_INSTALL_DIR = MOSIP_DIR
