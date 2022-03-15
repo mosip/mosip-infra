ca83ec18b6cbd902fc336b027b7d371a87ba08be
ckm007 <chandrakeshavmishra@gmail.com>
2022-03-15 07:53:42 +0000

    [MOSIP-20601] updated biosdk installation

--- deployment/v3/mosip/biosdk/install.sh
+++ deployment/v3/mosip/biosdk/install.sh
@@ -9,12 +9,14 @@ fi
 NS=biosdk
 CHART_VERSION=1.2.0
 
-echo Create namespace
-kubectl create ns $NS 
+echo Create $NS namespace
+kubectl create ns $NS
 
-echo Istio label 
+echo Istio label
 kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
 
 echo Installing Biosdk server
 helm -n $NS install biosdk-service mosip/biosdk-service -f values.yaml --version $CHART_VERSION
+
+echo Biosdk service installed sucessfully.
