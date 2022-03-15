0958660792e63439c9aa7f3721db6123b99d32fc
ckm007 <chandrakeshavmishra@gmail.com>
2022-03-15 06:53:12 +0000

    [MOSIP-20601] updated object-store installation part

--- deployment/v3/external/object-store/cred.sh
+++ deployment/v3/external/object-store/cred.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Creates configmap and secrets for S3/Minio
 # Specific "" for region for minio local installation
-[ $# -lt 3 ] && { echo "Usage: ./cred.sh <access key> <secret> <region> [kubeconfig]"; exit 1; }
 ## Usage: ./install.sh [kubeconfig]
 
 if [ $# -ge 3 ] ; then
@@ -10,11 +9,36 @@ fi
 
 NS=s3
 
-echo Create namespace
+echo Create $NS namespace
 kubectl create ns $NS 
 
 echo Istio label 
 kubectl label ns $NS istio-injection=enabled --overwrite
 
-kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$1 --from-literal=s3-region=$3 --dry-run=client  -o yaml | kubectl apply -f -
-kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$2 --dry-run=client  -o yaml | kubectl apply -f -
+echo Plesae select the type of object-store to be used:
+echo 1: for minio native using helm charts
+echo 2: for s3 object store
+read -p "Please choose the correct option as mentioned above(1/2) " choice
+if [ $choice = "1" ]
+then
+echo Creating secrets as per Minio native installation
+USER=$(kubectl -n minio get secret minio -o jsonpath='{.data.root-user}' | base64 --decode)
+PASS=$(kubectl -n minio get secret minio -o jsonpath='{.data.root-password}' | base64 --decode)
+kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$USER --from-literal=s3-region="" --dry-run=client  -o yaml | kubectl apply -f -
+kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$Pass --dry-run=client  -o yaml | kubectl apply -f -
+echo object-store secret and config map is set now.
+break
+elif [ $choice = "2" ]
+then
+read -p "Please enter the S3 user key " USER
+read -p "Please enter the S3 secret key" PASS
+read -p "Please enter the S3 region" REGION
+kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$USER --from-literal=s3-region=$REGION --dry-run=client  -o yaml | kubectl apply -f -
+kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$Pass --dry-run=client  -o yaml | kubectl apply -f -
+echo object-store secret and config map is set now.
+break
+else
+echo You entered wrong choice
+kill -9 `ps --pid $$ -oppid=`; exit
+break
+fi
--- deployment/v3/external/object-store/minio/install.sh
+++ deployment/v3/external/object-store/minio/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Installs MinIO inside cluster
+# Installs MinIO using helm chart inside MOSIP cluster
 ## Usage: ./install.sh [kubeconfig]
 
 if [ $# -ge 1 ] ; then
@@ -8,17 +8,17 @@ fi
 
 NS=minio
 
-echo Create namespace $NS
-kubectl create namespace $NS
+echo Create $NS namespace
+kubectl create ns $NS
 kubectl label ns $NS istio-injection=enabled --overwrite
 
 echo Installing minio
 helm -n minio install minio bitnami/minio --version 10.1.6 
 
-echo Installing gateways, vs
+echo Installing gateways and virtualservice
 EXTERNAL_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-minio-host})
 
 echo host: $EXTERNAL_HOST
 helm -n $NS install istio-addons chart/istio-addons --set externalHost=$EXTERNAL_HOST
 
-echo Helm installed. Now run ../cred.sh.
+echo Helm installed. Next step is to execute the cred.sh to update secrets in s3 namespace
