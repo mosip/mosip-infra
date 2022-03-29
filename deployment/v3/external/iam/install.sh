08939a4e0549a59089a2cc0bf4085bf79481bf3f
ckm007 <chandrakeshavmishra@gmail.com>
2022-03-29 09:20:40 +0530

    added wait in keycloak install.sh

--- deployment/v3/external/iam/install.sh
+++ deployment/v3/external/iam/install.sh
@@ -19,7 +19,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 echo Installing
-helm -n $NS install keycloak bitnami/keycloak --version "4.3.0" -f values.yaml
+helm -n $NS install keycloak bitnami/keycloak --version "4.3.0" -f values.yaml --wait
 
 EXTERNAL_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-iam-external-host})
 echo Install Istio gateway, virtual service
