fb8177b35e38d7fd393586f95678325a77e4b1a8
Puneet Joshi <puneet@mosip.io>
2022-03-31 15:10:46 +0530

    [MOSIP-20175] Version, Kibana added.

--- deployment/v3/mosip/landing-page/install.sh
+++ deployment/v3/mosip/landing-page/install.sh
@@ -19,6 +19,7 @@ helm repo update
 echo Copy configmaps
 ./copy_cm.sh
 
+VERSION=`git branch | grep "\*" | cut -d ' ' -f 2`
 NAME=$(kubectl get cm global -o jsonpath={.data.installation-name})
 DOMAIN=$(kubectl get cm global -o jsonpath={.data.installation-domain})
 API=$(kubectl get cm global -o jsonpath={.data.mosip-api-host})
@@ -26,6 +27,7 @@ API_INTERNAL=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host}
 ADMIN=$(kubectl get cm global -o jsonpath={.data.mosip-admin-host})
 PREREG=$(kubectl get cm global -o jsonpath={.data.mosip-prereg-host})
 KAFKA=$(kubectl get cm global -o jsonpath={.data.mosip-kafka-host})
+KIBANA=$(kubectl get cm global -o jsonpath={.data.mosip-kibana-host})
 ACTIVEMQ=$(kubectl get cm global -o jsonpath={.data.mosip-activemq-host})
 MINIO=$(kubectl get cm global -o jsonpath={.data.mosip-minio-host})
 KEYCLOAK=$(kubectl get cm global -o jsonpath={.data.mosip-iam-host})
@@ -34,12 +36,14 @@ POSTGRES_PORT=5432
 
 echo Installing landing page
 helm -n $NS install landing-page mosip/landing-page --version $CHART_VERSION  \
+--set landing.version=$VERSION \
 --set landing.name=$NAME \
 --set landing.api=$API \
 --set landing.apiInternal=$API_INTERNAL \
 --set landing.admin=$ADMIN  \
 --set landing.prereg=$PREREG  \
 --set landing.kafka=$KAFKA \
+--set landing.kibana=$KIBANA \
 --set landing.activemq=$ACTIVEMQ  \
 --set landing.minio=$MINIO \
 --set landing.keycloak=$KEYCLOAK  \
