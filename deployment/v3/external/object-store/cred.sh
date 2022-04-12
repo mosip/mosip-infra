9a1ba9bc7df52db0fb970fbd10460e47f7fd5599
Puneet Joshi <puneet@mosip.io>
2022-04-12 07:22:57 +0530

    But in cred.sh fixed.

--- deployment/v3/external/object-store/cred.sh
+++ deployment/v3/external/object-store/cred.sh
@@ -15,26 +15,26 @@ kubectl create ns $NS
 echo Istio label 
 kubectl label ns $NS istio-injection=enabled --overwrite
 
-echo Plesae select the type of object-store to be used:
-echo 1: for minio native using helm charts
-echo 2: for s3 object store
-read -p "Please choose the correct option as mentioned above(1/2) " choice
+echo Select the type of object-store to be used:
+echo 1: For minio native using helm charts
+echo 2: For any other s3 object store like AWS
+read -p "" choice
 if [ $choice = "1" ]
 then
 echo Creating secrets as per Minio native installation
 USER=$(kubectl -n minio get secret minio -o jsonpath='{.data.root-user}' | base64 --decode)
 PASS=$(kubectl -n minio get secret minio -o jsonpath='{.data.root-password}' | base64 --decode)
 kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$USER --from-literal=s3-region="" --dry-run=client  -o yaml | kubectl apply -f -
-kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$Pass --dry-run=client  -o yaml | kubectl apply -f -
+kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$PASS --dry-run=client  -o yaml | kubectl apply -f -
 echo object-store secret and config map is set now.
 break
 elif [ $choice = "2" ]
 then
-read -p "Please enter the S3 user key " USER
-read -p "Please enter the S3 secret key" PASS
-read -p "Please enter the S3 region" REGION
+read -p "Enter the S3 user key " USER
+read -p "Enter the S3 secret key" PASS
+read -p "Enter the S3 region" REGION
 kubectl -n s3 create configmap s3 --from-literal=s3-user-key=$USER --from-literal=s3-region=$REGION --dry-run=client  -o yaml | kubectl apply -f -
-kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$Pass --dry-run=client  -o yaml | kubectl apply -f -
+kubectl -n s3 create secret generic s3 --from-literal=s3-user-secret=$PASS --dry-run=client  -o yaml | kubectl apply -f -
 echo object-store secret and config map is set now.
 break
 else
