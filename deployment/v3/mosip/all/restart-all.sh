5a03d7cd5ef87173a096d8f33572dbc1f9daf455
Keshav Mishra <chandrakeshavmishra@gmail.com>
2022-04-13 14:22:51 +0530

    Update restart-all.sh

--- deployment/v3/mosip/all/restart-all.sh
+++ deployment/v3/mosip/all/restart-all.sh
@@ -27,7 +27,7 @@ declare -a module=("config-server"
                    "print"
                    "mosip-file-server"
                    "resident"
-                   "residetn-app"
+                   "resident-app"
                    "regclient"
                    )
 
