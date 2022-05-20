86098d2f50cb18abd54d5560ce8442a72dbfee98
Rakshith650 <rakshitham38@gmail.com>
2022-05-20 16:16:27 +0530

    [DSD-1009] updated the chart name and service name in delete.sh

--- deployment/v3/mosip/compliance-toolkit/delete.sh
+++ deployment/v3/mosip/compliance-toolkit/delete.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Uninstalls all  mosip-compliance-toolkit helm charts
+# Uninstalls all  compliance-toolkit helm charts
 while true; do
-    read -p "Are you sure you want to delete all mosip-compliance-toolkit helm charts?(Y/n) " yn
+    read -p "Are you sure you want to delete all compliance-toolkit helm charts?(Y/n) " yn
     if [ $yn = "Y" ]
       then
-        helm -n mosip-compliance-toolkit delete mosip-compliance-toolkit-gateway
-        helm -n $NS delete mosip-compliance-toolkit-ui
+        helm -n compliance-toolkit delete compliance-toolkit-gateway
+        helm -n $NS delete compliance-toolkit-ui compliance-toolkit
         break
       else
         break
