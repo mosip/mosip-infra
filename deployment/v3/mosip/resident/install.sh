345bfb905ea18610042b5224d44b8013a8bfe0da
Rakshith650 <rakshitham38@gmail.com>
2022-05-23 14:39:17 +0530

    [DSD-1009] updated the chart for resident-ui

--- deployment/v3/mosip/resident/install.sh
+++ deployment/v3/mosip/resident/install.sh
@@ -12,29 +12,30 @@ CHART_VERSION=12.0.2
 echo Create $NS namespace
 kubectl create ns $NS
 
-echo Istio label 
+echo Istio label
 kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
 
 echo Copy configmaps
 ./copy_cm.sh
 
-echo Installing Resident
-helm -n $NS install resident mosip/resident --version $CHART_VERSION
-
 API_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host})
-resident-ui_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-resident-ui-host})
+resident_ui_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-resident-ui-host})
+
+
+#echo Installing Resident
+helm -n $NS install resident mosip/resident --set istio.corsPolicy.allowOrigins.\[0\].prefix=$resident_ui_HOST --version $CHART_VERSION
 
 #echo Installing resident-ui-Proxy into Masterdata and Keymanager.
 #kubectl -n $NS apply -f resident-ui-proxy.yaml
 
 echo Installing resident-ui
-helm -n $NS install resident-ui mosip/resident-ui --set resident-ui.apiUrl=https://$API_HOST/v1/ --set istio.hosts\[0\]=$resident-ui_HOST --version $CHART_VERSION
+helm -n $NS install resident-ui mosip/resident-ui --set residentUi.apiHost=$API_HOST --set istio.hosts\[0\]=$resident_ui_HOST --version $CHART_VERSION
 
 kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
-echo Intalled resident services
+#echo Installed resident services
 
 echo Installed resident-ui
 
-echo "resident-ui portal URL: https://$resident-ui_HOST/resident-ui"
+echo "resident-ui portal URL: https://$resident_ui_HOST/resident-ui"
