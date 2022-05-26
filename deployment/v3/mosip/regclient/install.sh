bb7f458d6791eb73364e4112299b2d7f70fd4e32
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-05-26 21:03:07 +0530

    [ MOSIP-20295 ] Updated reg-client deployment scripts

--- deployment/v3/mosip/regclient/install.sh
+++ deployment/v3/mosip/regclient/install.sh
@@ -9,12 +9,26 @@ fi
 NS=regclient
 CHART_VERSION=12.0.2
 
+## GENERATE KEYSTORE PASSWORD
+KEYSTORE_PWD=$( openssl rand -base64 10 )
+
+bash create-signing-certs.sh $KEYSTORE_PWD
+
 echo Create $NS namespace
 kubectl create ns $NS
 
 echo Istio label
 kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
+
+
+echo "Create secret for keystore-secret-env, delete if exists"
+kubectl -n $NS delete secrets keystore-secret-env
+kubectl -n regclient create secret generic keystore-secret-env --from-literal="keystore_secret_env=$KEYSTORE_PWD"
+
+echo "Create configmaps for certs, delete if exists"
+kubectl -n $NS delete cm regclient-certs
+kubectl -n $NS create cm regclient-certs --from-file=./certs/
 
 echo Copy configmaps
 ./copy_cm.sh
@@ -29,6 +43,8 @@ helm -n $NS install regclient mosip/regclient \
   --set regclient.healthCheckUrl=$HEALTH_URL \
   --set regclient.hostName=$INTERNAL_HOST \
   --set istio.host=$REGCLIENT_HOST \
+  --set image.repository=mosipdev/registration-client \
+  --set image.tag=develop
   --wait \
   -f values.yaml \
   --version $CHART_VERSION
