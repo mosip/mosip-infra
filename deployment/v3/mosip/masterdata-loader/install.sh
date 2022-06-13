05bb27da35ff2e9a90e024d41625e1b335ab6ed3
Keshav Mishra <chandrakeshavmishra@gmail.com>
2022-06-13 14:16:22 +0530

    Update install.sh

--- deployment/v3/mosip/masterdata-loader/install.sh
+++ deployment/v3/mosip/masterdata-loader/install.sh
@@ -13,7 +13,9 @@ echo  Please skip this if masterdata is already uploaded.
 read -p "CAUTION: Do you still want to continue(Y/n)" yn
 if [ $yn = "Y" ] 
   then
-
+   NS=masterdata-loader
+   CHART_VERSION=12.0.1
+   helm delete masterdata-loader -n $NS
    echo Create $NS namespace
    helm delete masterdata-loader -n $NS
    kubectl create ns $NS
