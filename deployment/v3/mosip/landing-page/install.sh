7d9ae27bc2e69097a60cd02cad42648c8d0269fa
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-06-28 19:30:20 +0530

    [ DSD-1048 ] updated compliance toolkit and landing-page deployment scripts

--- deployment/v3/mosip/landing-page/install.sh
+++ deployment/v3/mosip/landing-page/install.sh
@@ -34,6 +34,7 @@ KEYCLOAK=$(kubectl get cm global -o jsonpath={.data.mosip-iam-external-host})
 REGCLIENT=$(kubectl get cm global -o jsonpath={.data.mosip-regclient-host})
 POSTGRES=$(kubectl get cm global -o jsonpath={.data.mosip-postgres-host})
 POSTGRES_PORT=5432
+PMP=$(kubectl get cm global -o jsonpath={.data.mosip-pmp-host})
 COMPLIANCE=$(kubectl get cm global -o jsonpath={.data.mosip-compliance-host})
 
 echo Installing landing page
@@ -53,6 +54,7 @@ helm -n $NS install landing-page mosip/landing-page --version $CHART_VERSION  \
 --set landing.postgres.host=$POSTGRES \
 --set landing.postgres.port=$POSTGRES_PORT \
 --set landing.compliance=$COMPLIANCE \
+--set landing.pmp=$PMP \
 --set istio.host=$DOMAIN
 
 kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
