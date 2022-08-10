a2eb8c2ccb90e3d5c00fcaabe33d3eed4bee4a07
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-08-10 21:08:00 +0530

    [ DSD-1316 ] Added deployment scripts for digital card service

---
+++ deployment/v3/mosip/digitalcard/restart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Restart the digitalcard
+## Usage: ./restart.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+
+NS=digitalcard
+kubectl -n $NS rollout restart deploy
+
+kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
+
+echo Retarted digitalcard services
