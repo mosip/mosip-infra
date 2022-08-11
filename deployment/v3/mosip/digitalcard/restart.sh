769dd3e02bbcbd9f98b473fe1f266ae7e5d76c40
Akila Lakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2022-08-11 10:54:15 +0530

    Corrected spell check in restart script.

--- deployment/v3/mosip/digitalcard/restart.sh
+++ deployment/v3/mosip/digitalcard/restart.sh
@@ -12,4 +12,4 @@ kubectl -n $NS rollout restart deploy
 
 kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
-echo Retarted digitalcard services
+echo Restarted digitalcard services
