5f8b40bb266ae5317945e77d3f8ea6042cfa1aef
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-08-22 20:56:48 +0530

    [ MOSIP-23652 ] set values for mosip-file-server allowOrigins

--- deployment/v3/mosip/mosip-file-server/install.sh
+++ deployment/v3/mosip/mosip-file-server/install.sh
@@ -21,6 +21,8 @@ echo Copy configmaps
 ./copy_cm.sh
 
 FILESERVER_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-host})
+API_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-host})
+API_INTERNAL_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host})
 HEALTH_URL=https://$FILESERVER_HOST/.well-known/
 
 read -p "Please Enter MOBILE APP Link publicly accessible APK: " pub_url
@@ -31,8 +33,10 @@ helm -n $NS install mosip-file-server mosip/mosip-file-server      \
   --set mosipfileserver.host=$FILESERVER_HOST                      \
   --set mosipfileserver.puburl={$pub_url}                          \
   --set mosipfileserver.privurl={$priv_url}                        \
+  --set istio.corsPolicy.allowOrigins\[0\]=$API_HOST               \
+  --set istio.corsPolicy.allowOrigins\[1\]=$API_INTERNAL_HOST      \
+  --set istio.corsPolicy.allowOrigins\[2\].prefix=https://verifiablecredential.io \
   --wait                                                           \
-  -f values.yaml                                                   \
   --version $CHART_VERSION
 
 echo Get your download url from here
