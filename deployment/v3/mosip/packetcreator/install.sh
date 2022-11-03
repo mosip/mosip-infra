fbcc2c44b2d8db681346f242ea5d8f76f40725b1
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-11-03 17:30:00 +0530

    [ MOSIP-24255 ] updated install.sh

--- deployment/v3/mosip/packetcreator/install.sh
+++ deployment/v3/mosip/packetcreator/install.sh
@@ -9,24 +9,34 @@ fi
 NS=packetcreator
 CHART_VERSION=12.0.2
 
-read -p "Provide the allowed origin list separated by comma : " originList;
-
-list='';
-i=0;
-originList=$( echo "$originList" | sed 's/,/ /g');
-for origin in $originList; do
-  list="$list --set istio.corsPolicy.allowOrigins[$i].prefix=$origin ";
-  i=$(( i+1 ));
-done
-
 echo Create $NS namespace
 kubectl create ns $NS
 
-echo Istio label
-kubectl label ns $NS istio-injection=enabled --overwrite
-helm repo update
+echo "Select the type of Ingress controller to be used (1/2): ";
+echo "1. Ingress";
+echo "2. Istio";
+read -p "" choice
+
+if [ $choice = "1" ]; then
+  list="--set ingress.enabled=true";
+fi
+
+if [ $choice = "2" ]; then
+  read -p "Provide the allowed origin list separated by comma : " originList;
+  list='--set istio.enabled=true';
+  i=0;
+  originList=$( echo "$originList" | sed 's/,/ /g');
+  for origin in $originList; do
+    list="$list --set istio.corsPolicy.allowOrigins[$i].prefix=$origin ";
+    i=$(( i+1 ));
+  done
+
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
+  helm repo update
+fi
 
 echo Installing packetcreator
-helm -n $NS install packetcreator mosip/packetcreator $( echo $list ) --wait --version $CHART_VERSION
+helm -n $NS install packetcreator /home/techno-384/Desktop/MOSIP/mosip-helm/charts/packetcreator $( echo $list ) --wait --version $CHART_VERSION
 echo Installed packetcreator.
 
