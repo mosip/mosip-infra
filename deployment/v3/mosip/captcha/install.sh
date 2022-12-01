924cedecc2ac49915eb4c2275689ff0a04005941
pramodbelal8@gmail.com <pramodbelal8@gmail.com>
2022-12-01 16:08:00 +0530

    [DSD-1692] Updated mosip captch in to generate mosip-captcha site&secret keys.

--- deployment/v3/mosip/captcha/install.sh
+++ deployment/v3/mosip/captcha/install.sh
@@ -17,5 +17,11 @@ read SITE_KEY
 echo Please enter the recaptcha admin secret key for domain $PREREG_HOST
 read SECRET_KEY
 
+echo Please enter the recaptcha admin site key for domain $MOSIP_HOST
+read MSITE_KEY
+echo Please enter the recaptcha admin secret key for domain $MOSIP_HOST
+read MSECRET_KEY
+
 echo Setting up captcha secrets
 kubectl -n $NS create secret generic prereg-captcha --from-literal=prereg-captcha-site-key=$SITE_KEY --from-literal=prereg-captcha-secret-key=$SECRET_KEY --dry-run=client  -o yaml | kubectl apply -f -
+kubectl create secret generic mosip-captcha --from-literal=mosip-captcha-site-key=$MSITE_KEY --from-literal=mosip-captcha-secret-key=$MSECRET_KEY --dry-run=client  -o yaml | kubectl apply -f -
