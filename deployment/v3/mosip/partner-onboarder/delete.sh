2d6365fe7b32172728bcea7e4bd05a8609b1a968
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-12-13 19:50:55 +0530

    [ MOSIP-25231 ] updated install.sh & delete.sh for partner-onboarder

--- deployment/v3/mosip/partner-onboarder/delete.sh
+++ deployment/v3/mosip/partner-onboarder/delete.sh
@@ -6,6 +6,12 @@ if [ $# -ge 1 ] ; then
   export KUBECONFIG=$1
 fi
 NS=onboarder
-echo Deleting partner-onboarder helm
-kubectl -n $NS delete configmap global
-helm -n $NS delete partner-onboarder
+while true; do
+    read -p "Are you sure you want to delete all partner-onboarder ?(Y/n) " yn
+    if [ $yn = "Y" ]; then
+      echo Deleting partner-onboarder helm
+      kubectl -n $NS --ignore-not-found=true  delete configmap global
+      helm -n $NS delete partner-onboarder
+      break
+    fi
+done
