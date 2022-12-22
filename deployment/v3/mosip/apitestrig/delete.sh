d1981e684ee12e6c9c0aef35f62bb1b318bd87ae
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-12-22 18:56:14 +0530

    [ MOSIP-25254 ] updated scripts to run api-testrig for self-signed env

--- deployment/v3/mosip/apitestrig/delete.sh
+++ deployment/v3/mosip/apitestrig/delete.sh
@@ -11,8 +11,6 @@ while true; do
     read -p "Are you sure you want to delete apitestrig helm charts?(Y/n) " yn
     if [ $yn = "Y" ]
       then
-        kubectl -n $NS delete configmap db-cm global keycloak-host s3 apitestrig
-        kubectl -n $NS delete secret db-secrets keycloak-client-secrets s3
         helm -n $NS delete apitestrig
         break
       else
