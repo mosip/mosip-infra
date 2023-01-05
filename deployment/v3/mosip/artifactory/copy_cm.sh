140c4078b39d0bb4e1685c887228920cc5945325
syed-salman-technoforte <syed.salman@technoforte.co.in>
2023-01-05 11:46:28 +0530

    [ DSD-1944 ] updated artifactory install.sh and removed copy_cm.sh

--- deployment/v3/mosip/artifactory/copy_cm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copy configmaps.
-# DST_NS: Destination namespace 
-
-COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=artifactory
-
-$COPY_UTIL configmap artifactory-share $DST_NS  $DST_NS artifactory-share-develop
