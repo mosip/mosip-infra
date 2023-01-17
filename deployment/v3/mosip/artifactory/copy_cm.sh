aff8793cb8d7bc31fa9ac590f38825124db5f44d
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-01-17 12:49:01 +0530

    Delete copy_cm.sh

--- deployment/v3/mosip/artifactory/copy_cm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copy configmaps.
-# DST_NS: Destination namespace 
-
-COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=artifactory
-
-$COPY_UTIL configmap artifactory-share $DST_NS  $DST_NS artifactory-share-beta1
