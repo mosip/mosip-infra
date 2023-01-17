b8f74147ba46653a83fbf5376d489959df811cce
Rakshithb1 <rakshit.b@technoforte.co.in>
2023-01-17 19:05:41 +0530

    [DSD-2014] updated mimoto chat version

--- deployment/v3/mosip/resident/install.sh
+++ deployment/v3/mosip/resident/install.sh
@@ -7,7 +7,8 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=resident
-CHART_VERSION=12.0.1-B3
+CHART_VERSION=12.0.1-B2
+MIMOTO_CHART_VERSION=12.0.1-B3
 
 echo Create $NS namespace
 kubectl create ns $NS
@@ -32,7 +33,7 @@ function installing_resident() {
   helm -n $NS install resident mosip/resident --set istio.corsPolicy.allowOrigins\[0\].prefix=$RESIDENT_HOST --version $CHART_VERSION
 
   echo Installing mimoto
-  helm -n $NS install mimoto mosip/mimoto --version $CHART_VERSION
+  helm -n $NS install mimoto mosip/mimoto --version $MIMOTO_CHART_VERSION
 
   echo Installing Resident UI
   helm -n $NS install resident-ui mosip/resident-ui --set resident.apiHost=$API_HOST --set istio.hosts\[0\]=$RESIDENT_HOST --version $CHART_VERSION
