2f556be2eb369e0037ccb2b8de87dd8e3b7b9c24
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-01-22 12:17:22 +0530

    Update install.sh

--- deployment/v3/mosip/artifactory/install.sh
+++ deployment/v3/mosip/artifactory/install.sh
@@ -18,7 +18,7 @@ function installing_artifactory() {
   helm repo update
 
   echo Installing artifactory
-  helm -n $NS install artifactory mosip/artifactory --version $CHART_VERSION
+  helm -n $NS install artifactory mosip/artifactory --set image.repository mosipdev/artifactory-server --version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
