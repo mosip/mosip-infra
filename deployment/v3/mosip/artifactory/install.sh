0a4f23e6db6ba3049b5621e7c5672c36d7a91320
dell <gaganaa10@gmail.com>
2023-01-30 12:55:11 +0530

    [MOSIP-25972] Updated install.sh scripts for qa-data branch

--- deployment/v3/mosip/artifactory/install.sh
+++ deployment/v3/mosip/artifactory/install.sh
@@ -17,7 +17,7 @@ kubectl label ns $NS istio-injection=enabled --overwrite
 helm repo update
 
 echo Installing artifactory
-helm -n $NS install artifactory mosip/artifactory --version $CHART_VERSION 
+helm -n $NS install artifactory mosip/artifactory --set image.tag=1.2.0.1-B2 --version $CHART_VERSION
 
 kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
