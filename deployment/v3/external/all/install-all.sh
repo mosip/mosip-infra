c2522ffb88988243ff7432463b1e7a2a45eb0a99
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-03-15 11:50:58 +0530

    Mosip 25304 (#1246)
    * [MOSIP-25304] moved landing page to external
    
    * [MOSIP-25304] renamed activemq
    
    * [MOSIP-25304] updated docs for activemq and landing change
    
    * [MOSIP-25304] moved docker-secrets, captcha to externel
    
    * [MOSIP-25304] restructured aptestrig
    
    * Update install-all.sh
    
    * Update install-all.sh

--- deployment/v3/external/all/install-all.sh
+++ deployment/v3/external/all/install-all.sh
@@ -31,7 +31,7 @@ function installing_all() {
   cd $ROOT_DIR/antivirus/clamav
   ./install.sh
 
-  cd $ROOT_DIR/activemq/aws
+  cd $ROOT_DIR/activemq
   ./install.sh
 
   cd $ROOT_DIR/kafka
@@ -44,6 +44,18 @@ function installing_all() {
   cd $ROOT_DIR/msg-gateway
   ./install.sh
 
+  cd $ROOT_DIR/docker-secrets
+  ./install.sh
+
+  cd $ROOT_DIR/conf-secrets
+  ./install.sh
+
+  cd $ROOT_DIR/landing-page
+  ./install.sh
+  
+  cd $ROOT_DIR/captcha
+  ./install.sh
+  
   echo All External services deployed an configured sucessfully.
   return 0
 }
