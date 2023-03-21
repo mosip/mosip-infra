7e3e0770db0f2147e1f23864bb6d52fd3c3ca4f5
Mohan E <mohanraj1715@gmail.com>
2023-03-21 20:14:36 +0530

    updated install.sh script

--- deployment/v3/mosip/resident/install.sh
+++ deployment/v3/mosip/resident/install.sh
@@ -31,13 +31,13 @@ function installing_resident() {
 
 
   echo Installing Resident
-  helm -n $NS install resident mosip/resident --set istio.corsPolicy.allowOrigins\[0\].prefix=$RESIDENT_HOST --version $CHART_VERSION
+  helm -n $NS install resident mosip/resident --set istio.corsPolicy.allowOrigins\[0\].prefix=https://$RESIDENT_HOST --version $CHART_VERSION
 
   echo Installing mimoto
   helm -n $NS install mimoto mosip/mimoto --version $MIMOTO_CHART_VERSION
 
   echo Installing Resident UI
-  helm -n $NS install resident-ui mosip/resident-ui --set resident.apiHost=$API_HOST --set istio.hosts\[0\]=$RESIDENT_HOST --version $CHART_VERSION
+  helm -n $NS install resident-ui mosip/resident-ui --set resident.apiHost=$API_HOST --set istio.hosts\[0\]=https://$RESIDENT_HOST --version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
