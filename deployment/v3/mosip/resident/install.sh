98d6942a4bfb108cf3c14680e5d1b1ef0427d798
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-03-30 12:34:06 +0530

    [ DSD-2389 ] updated chart version to 12.0.1-B3 release (#1266)
    * [ DSD-2389 ] updated chart version to 12.0.1-B3 release
    
    * [ DSD-2389 ] updated chart version for resident ui
    
    ---------
    
    Co-authored-by: syed-salman-technoforte <syed.salman@technoforte.co.in>

--- deployment/v3/mosip/resident/install.sh
+++ deployment/v3/mosip/resident/install.sh
@@ -7,8 +7,8 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=resident
-CHART_VERSION=12.0.1-B2
-MIMOTO_CHART_VERSION=12.0.1-B3
+CHART_VERSION=12.0.1-B3
+RESIDENT_UI_CHART_VERSION=0.0.1
 
 echo Create $NS namespace
 kubectl create ns $NS
@@ -55,10 +55,10 @@ function installing_resident() {
   helm -n $NS install resident mosip/resident --set istio.corsPolicy.allowOrigins\[0\].prefix=https://$RESIDENT_HOST --version $CHART_VERSION $ENABLE_INSECURE
 
   echo Installing mimoto
-  helm -n $NS install mimoto mosip/mimoto --version $MIMOTO_CHART_VERSION
+  helm -n $NS install mimoto mosip/mimoto --version $CHART_VERSION
 
   echo Installing Resident UI
-  helm -n $NS install resident-ui mosip/resident-ui --set resident.apiHost=$API_HOST --set istio.hosts\[0\]=$RESIDENT_HOST --version $CHART_VERSION
+  helm -n $NS install resident-ui mosip/resident-ui --set resident.apiHost=$API_HOST --set istio.hosts\[0\]=$RESIDENT_HOST --version $RESIDENT_UI_CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
