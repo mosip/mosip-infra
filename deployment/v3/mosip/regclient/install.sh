a5f2184c3a91ac4cccbaa14976133cbf0c2304d0
Mohan E <mohanraj1715@gmail.com>
2023-04-05 09:42:52 +0530

    [ DSD-2423 ] Updated the client_upgrade_server_url value. (#1273)

--- deployment/v3/mosip/regclient/install.sh
+++ deployment/v3/mosip/regclient/install.sh
@@ -40,7 +40,7 @@ function installing_regclient() {
 
   echo Installing reg client downloader. This may take a few minutes ..
   helm -n $NS install regclient mosip/regclient \
-    --set regclient.upgradeServerUrl=https://$INTERNAL_HOST \
+    --set regclient.upgradeServerUrl=https://$REGCLIENT_HOST \
     --set regclient.healthCheckUrl=$HEALTH_URL \
     --set regclient.hostName=$INTERNAL_HOST \
     --set istio.host=$REGCLIENT_HOST \
