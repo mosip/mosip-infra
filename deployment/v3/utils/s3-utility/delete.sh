ce0d6c364807bce8be29c1f9f0f345bc5b641c27
akilalakshmanan <Akila.L@iiitb.ac.in>
2023-04-10 16:47:48 +0530

    [DSD-2445] Updated s3 utility installation scripts

--- deployment/v3/utils/s3-utility/delete.sh
+++ deployment/v3/utils/s3-utility/delete.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Uninstalls minio-client
+# Uninstalls s3-utility
 ## Usage: ./delete.sh [kubeconfig]
 
 if [ $# -ge 1 ] ; then
   export KUBECONFIG=$1
 fi
 
-function deleting_minio-client() {
-  NS=minio-client
+function deleting_s3-utility() {
+  NS=s3-utility
   while true; do
-      read -p "Are you sure you want to delete minio-client helm charts?(Y/n) " yn
+      read -p "Are you sure you want to delete s3-utility helm charts?(Y/n) " yn
       if [ $yn = "Y" ]
         then
-          helm -n $NS delete minio-client
+          helm -n $NS delete s3-utility
           break
         else
           break
@@ -27,4 +27,4 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errtrace  # trace ERR through 'time command' and other functions
 set -o pipefail  # trace ERR through pipes
-deleting_minio-client   # calling function
+deleting_s3-utility   # calling function
