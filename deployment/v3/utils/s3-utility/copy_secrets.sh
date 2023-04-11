d2c6fc6b77c3d6e6faaee47c25f237bbc834dc9f
Akila Lakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2023-04-11 16:21:31 +0530

    [DSD-2445] Updated s3 utility installation scripts (#1277)
    * [DSD-2445] Updated s3 utility installation scripts
    
    * [DSD-2445]
    
    * [DSD-2445]
    
    * [DSD-2445]
    
    ---------
    
    Co-authored-by: Keshav Mishra <chandrakeshavmishra@gmail.com>

--- deployment/v3/utils/s3-utility/copy_secrets.sh
+++ deployment/v3/utils/s3-utility/copy_secrets.sh
@@ -2,6 +2,6 @@
 # Copy secrets from other namespaces
 # DST_NS: Destination namespace 
 COPY_UTIL=../copy_cm_func.sh
-DST_NS=minio-client
+DST_NS=util
 $COPY_UTIL secret s3 s3 $DST_NS
 
--- deployment/v3/utils/s3-utility/install.sh
+++ deployment/v3/utils/s3-utility/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# Installs minio-client
+# Installs s3-utility
 ## Usage: ./install.sh [kubeconfig]
 
 if [ $# -ge 1 ] ; then
   export KUBECONFIG=$1
 fi
 
-NS=minio-client
+NS=util
 CHART_VERSION=1.0.0
 
 echo Create $NS namespace
 kubectl create ns $NS
 
 
-function installing_minio-client() {
+function installing_s3-utility() {
   echo Istio label
   kubectl label ns $NS istio-injection=disabled --overwrite
   helm repo update
@@ -35,18 +35,18 @@ function installing_minio-client() {
      exit 1;
   fi
 
-  read -p "Please provide number of days the apitestrig reports needed to be cleared from minio [format:'no_of_days'd](eg:3d) : " NO_OF_DAYS
-  if [ -z "$NO_OF_DAYS" ]; then
+  read -p "Please provide number of days the apitestrig reports needed to be cleared from minio [format:'no_of_days'd](eg:3d) : " S3_RETENTION_DAYS
+  if [ -z "$S3_RETENTION_DAYS" ]; then
       echo "ERROR: Number of days to clear the test report cannot be empty; EXITING;";
       exit 1;
   fi
 
-  echo Installing minio-client
-  helm -n $NS install minio-client mosip/minio-client --set minioclient.no_of_days=$NO_OF_DAYS \
+  echo Installing s3-utility
+  helm -n $NS install s3-utility mosip/s3-utility --set minioclient.retention_days=$S3_RETENTION_DAYS \
   --set crontime="0 $time * * *" \
   --version $CHART_VERSION
   
-  echo Installed minio-client utility
+  echo Installed s3 client utility
   return 0
 }
 
@@ -56,4 +56,4 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errtrace  # trace ERR through 'time command' and other functions
 set -o pipefail  # trace ERR through pipes
-installing_minio-client  # calling function
+installing_s3-utility  # calling function
