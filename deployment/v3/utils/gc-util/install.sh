d3a43ece6a64439930bbd900461e3027392e3fbe
akilalakshmanan <Akila.L@iiitb.ac.in>
2023-04-24 16:17:52 +0530

    [DSD-2431] Added installation scripts for garbage cleanup utility

---
+++ deployment/v3/utils/gc-util/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Installs gc-util
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=gc-util
+CHART_VERSION=1.0.0
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+
+function installing_gc-util() {
+  helm repo update
+
+  read -p "Please enter the time(hr) to run the cronjob every day (time: 0-23) : " time
+  if [ -z "$time" ]; then
+     echo "ERROR: Time cannot be empty; EXITING;";
+     exit 1;
+  fi
+  if ! [ $time -eq $time ] 2>/dev/null; then
+     echo "ERROR: Time $time is not a number; EXITING;";
+     exit 1;
+  fi
+  if [ $time -gt 23 ] || [ $time -lt 0 ] ; then
+     echo "ERROR: Time should be in range ( 0-23 ); EXITING;";
+     exit 1;
+  fi
+
+  echo Installing gc-util
+  helm -n $NS install gc-util mosip/gc-util --set crontime="0 $time * * *" \
+  --version $CHART_VERSION
+
+  echo Installed garbage cleanup utility
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_gc-util  # calling function
