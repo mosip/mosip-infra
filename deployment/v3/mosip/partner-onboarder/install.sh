4767ca55482ef11dc5ef2efa1351754d0b030e7e
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-05-15 20:52:41 +0530

    [MOSIP-24332] Update install.sh

--- deployment/v3/mosip/partner-onboarder/install.sh
+++ deployment/v3/mosip/partner-onboarder/install.sh
@@ -8,7 +8,7 @@ fi
 
 echo "Do you have public domain & valid SSL? (Y/n) "
 echo "Y: if you have public domain & valid ssl certificate"
-echo "n: if you don't have public domain & valid ssl certificate"
+echo "n: If you don't have a public domain and a valid SSL certificate. Note: It is recommended to use this option only in development environments."
 read -p "" flag
 
 if [ -z "$flag" ]; then
