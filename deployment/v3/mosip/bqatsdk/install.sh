aeaf1d172d60b7de7ffad27fbee665cf806679de
bhumi46 <thisisbn46@gmail.com>
2023-05-29 13:34:39 +0530

    updated copy.sh and install. sh

--- deployment/v3/mosip/bqatsdk/install.sh
+++ deployment/v3/mosip/bqatsdk/install.sh
@@ -16,13 +16,13 @@ function installing_bqatsdk() {
   echo Istio label
   kubectl label ns $NS istio-injection=enabled --overwrite
   helm repo update
-  
+
   echo Copy configmaps
   sed -i 's/\r$//' copy_cm.sh
   ./copy_cm.sh
 
   echo Installing Bqatsdk server
-  helm -n $NS install bqat-sdk /home/bhuminathan/develop/helm/mosip-helm/charts/bqat-sdk -f values.yaml --version $CHART_VERSION
+  helm -n $NS install bqat-sdk mosip/bqat-sdk -f values.yaml --version $CHART_VERSION
 
   echo Bqatsdk service installed sucessfully.
   return 0
