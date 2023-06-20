2e39e6fe39c6fbb8603ea4c9baa0d16f10241977
Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>
2023-06-20 14:21:22 +0530

    [MOSIP-28053]fixed the issue
    Signed-off-by: Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>

--- deployment/v3/testrig/dslrig/copy_cm.sh
+++ deployment/v3/testrig/dslrig/copy_cm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copy configmaps from other namespaces
 # DST_NS: Destination namespace 
-COPY_UTIL=../utils/copy_cm_func.sh
+COPY_UTIL=../../utils/copy_cm_func.sh
 DST_NS=dslrig
 $COPY_UTIL configmap global default $DST_NS 
 $COPY_UTIL configmap keycloak-host keycloak $DST_NS
