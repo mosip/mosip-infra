77da7c35bcff758b066d4738f17e996b5e3412b0
Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>
2023-06-20 14:21:54 +0530

    [MOSIP-28053]fixed the issue
    Signed-off-by: Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>

--- deployment/v3/testrig/dslrig/copy_secrets.sh
+++ deployment/v3/testrig/dslrig/copy_secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copy secrets from other namespaces
 # DST_NS: Destination namespace 
-COPY_UTIL=../utils/copy_cm_func.sh
+COPY_UTIL=../../utils/copy_cm_func.sh
 DST_NS=dslrig
 $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
 $COPY_UTIL secret s3 s3 $DST_NS
