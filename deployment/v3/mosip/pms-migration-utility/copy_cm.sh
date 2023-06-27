d4cf1a7b35db85fa69fe50bf6bcba41dabd0cdc9
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2023-06-27 14:31:06 +0530

    [DSD-2771] Updated pms-migration-utility deployment scripts (#1348)
    Co-authored-by: syed-salman-technoforte <syed.salman@technoforte.co.in>

--- deployment/v3/mosip/pms-migration-utility/copy_cm.sh
+++ deployment/v3/mosip/pms-migration-utility/copy_cm.sh
@@ -3,9 +3,8 @@
 # DST_NS: Destination namespace
 
 COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=upgrade
+DST_NS=pms-migration-utility
 
 $COPY_UTIL configmap global default $DST_NS
 $COPY_UTIL configmap artifactory-share artifactory $DST_NS
 $COPY_UTIL configmap config-server-share config-server $DST_NS
-$COPY_UTIL configmap softhsm-kernel-share softhsm $DST_NS
