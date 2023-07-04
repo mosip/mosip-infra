d416409a348c7835ca9722bf5f0b0296bc15044b
Rakshithb1 <rakshit.b@technoforte.co.in>
2023-07-04 17:43:08 +0530

    [MOSIP-27549] added error handling in copy_cm.sh and copy_secrets.sh scripts

--- deployment/v3/mosip/pms/copy_cm.sh
+++ deployment/v3/mosip/pms/copy_cm.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Copy configmaps from other namespaces
-# DST_NS: Destination namespace 
+# DST_NS: Destination namespace
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
 
 COPY_UTIL=../../utils/copy_cm_func.sh
 DST_NS=pms
