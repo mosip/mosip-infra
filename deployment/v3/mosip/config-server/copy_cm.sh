4ef948b8e77e4cb13b32394b4196bec96a0900e8
Rakshithb1 <rakshit.b@technoforte.co.in>
2023-07-11 12:25:42 +0530

    [MOSIP-27549] added error handling in copy_cm.sh and copy_secrets.sh … (#1357)
    * [MOSIP-27549] added error handling in copy_cm.sh and copy_secrets.sh scripts
    
    * [MOSIP-27549] added error handling in copy_cm.sh and copy_secrets.sh scripts
    
    * [MOSIP-27549] updated copy_cm.sh and copy_secrets.sh scripts with functions
    
    * [MOSIP-27549] updated scripts

--- deployment/v3/mosip/config-server/copy_cm.sh
+++ deployment/v3/mosip/config-server/copy_cm.sh
@@ -2,10 +2,21 @@
 # Copy configmaps from other namespaces
 # DST_NS: Destination namespace
 
-COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=config-server
-$COPY_UTIL configmap global default $DST_NS 
-$COPY_UTIL configmap keycloak-host keycloak $DST_NS 
-$COPY_UTIL configmap activemq-activemq-artemis-share activemq $DST_NS
-$COPY_UTIL configmap s3 s3 $DST_NS
-$COPY_UTIL configmap email-gateway msg-gateways $DST_NS
+function copying_cm() {
+  COPY_UTIL=../../utils/copy_cm_func.sh
+  DST_NS=config-server
+  $COPY_UTIL configmap global default $DST_NS
+  $COPY_UTIL configmap keycloak-host keycloak $DST_NS
+  $COPY_UTIL configmap activemq-activemq-artemis-share activemq $DST_NS
+  $COPY_UTIL configmap s3 s3 $DST_NS
+  $COPY_UTIL configmap email-gateway msg-gateways $DST_NS
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+copying_cm   # calling function
