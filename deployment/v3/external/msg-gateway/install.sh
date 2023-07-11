67dca60ce65ab0b72ee12dee6b3b931a6344f5ca
Rakshitha650 <rakshitham38@gmail.com>
2023-07-11 11:30:43 +0530

    [DSD-2651] updated the sms

--- deployment/v3/external/msg-gateway/install.sh
+++ deployment/v3/external/msg-gateway/install.sh
@@ -25,22 +25,26 @@ function msg_gateway() {
   SMS_SECRET="''"
 
   read -p "Would you like to use mock-smtp (Y/N) [ Default: Y ] : " yn
-  read -p "Would you like to use mock-sms (Y/N) [ Default: Y ] : " yn
   # Set yn to N if user input is null
   if [ -z $yn ]; then
     yn=Y;
   fi
-  if [ $yn != "Y" ]; then
+  if [ $yn = "Y" ]; then
       read -p "Please enter the SMTP host " SMTP_HOST
       read -p "Please enter the SMTP host port " SMTP_PORT
       read -p "Please enter the SMTP user " SMTP_USER
       read -p "Please enter the SMTP secret key " SMTP_SECRET
+  fi
+  read -p "Would you like to use mock-sms (Y/N) [ Default: Y ] : " yn
+  if [ -z $yn ]; then
+    yn=Y;
+  fi
+  if [ $yn = "Y" ]; then
       read -p "Please enter the SMS host " SMS_HOST
       read -p "Please enter the SMS host port " SMS_PORT
       read -p "Please enter the SMS user " SMS_USER
       read -p "Please enter the SMS secret key " SMS_SECRET
   fi
-
   kubectl -n $NS delete --ignore-not-found=true configmap msg-gateway
   kubectl -n $NS create configmap msg-gateway --from-literal="smtp-host=$SMTP_HOST" --from-literal="sms-host=$SMS_HOST" --from-literal="smtp-port=$SMTP_PORT" --from-literal="sms-port=$SMS_PORT" --from-literal="smtp-username=$SMTP_USER" --from-literal="sms-username=$SMS_USER"
   kubectl -n $NS delete --ignore-not-found=true secret msg-gateway
@@ -49,7 +53,6 @@ function msg_gateway() {
   echo smtp and sms realted configurations set.
   return 0
 }
-
 # set commands for error handling.
 set -e
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
