b90f2d92dc80da18eac8b00da7c51adb146ad8cf
Rakshitha650 <rakshitham38@gmail.com>
2023-07-11 14:10:19 +0530

    [DSD-2651]added sms and email configuration (#1358)
    * [DSD-2651]added sms and email configuration
    
    * [DSD-2651]replaced sms with email
    
    * [DSD-2651]replaced sms with email
    
    * [DSD-2651]replaced sms with email
    
    * [DSD-2651]replaced sms with email
    
    * [DSD-2651]replaced sms with email
    
    * [DSD-2651]
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the sms
    
    * [DSD-2651] updated the sms
    
    * [DSD-2651] updated the sms
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    * [DSD-2651] updated the README.md
    
    ---------
    
    Signed-off-by: Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>

--- deployment/v3/mosip/config-server/copy_cm.sh
+++ deployment/v3/mosip/config-server/copy_cm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Copy configmaps from other namespaces
-# DST_NS: Destination namespace
-
-COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=config-server
-$COPY_UTIL configmap global default $DST_NS 
-$COPY_UTIL configmap keycloak-host keycloak $DST_NS 
-$COPY_UTIL configmap activemq-activemq-artemis-share activemq $DST_NS
-$COPY_UTIL configmap s3 s3 $DST_NS
-$COPY_UTIL configmap email-gateway msg-gateways $DST_NS
+function copying_cm() {
+  COPY_UTIL=../../utils/copy_cm_func.sh
+  DST_NS=config-server # DST_NS: Destination namespace
+  $COPY_UTIL configmap global default $DST_NS
+  $COPY_UTIL configmap keycloak-host keycloak $DST_NS
+  $COPY_UTIL configmap activemq-activemq-artemis-share activemq $DST_NS
+  $COPY_UTIL configmap s3 s3 $DST_NS
+  $COPY_UTIL configmap email-gateway msg-gateways $DST_NS
+  return 0
+}
--- deployment/v3/mosip/config-server/copy_secrets.sh
+++ deployment/v3/mosip/config-server/copy_secrets.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Copy secrets from other namespaces
-# DST_NS: Destination namespace
 
-COPY_UTIL=../../utils/copy_cm_func.sh
-DST_NS=config-server
-$COPY_UTIL secret db-common-secrets postgres $DST_NS 
-$COPY_UTIL secret keycloak keycloak $DST_NS 
-$COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
-$COPY_UTIL secret activemq-activemq-artemis activemq $DST_NS 
-$COPY_UTIL secret softhsm-kernel softhsm $DST_NS
-$COPY_UTIL secret softhsm-ida softhsm $DST_NS
-$COPY_UTIL secret s3 s3 $DST_NS
-$COPY_UTIL secret email-gateway msg-gateways $DST_NS
-$COPY_UTIL secret mosip-captcha captcha $DST_NS
-$COPY_UTIL secret conf-secrets-various conf-secrets $DST_NS
+function copying_secrets() {
+  COPY_UTIL=../../utils/copy_cm_func.sh
+  DST_NS=config-server  # DST_NS: Destination namespace
+  $COPY_UTIL secret db-common-secrets postgres $DST_NS
+  $COPY_UTIL secret keycloak keycloak $DST_NS
+  $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
+  $COPY_UTIL secret activemq-activemq-artemis activemq $DST_NS
+  $COPY_UTIL secret softhsm-kernel softhsm $DST_NS
+  $COPY_UTIL secret softhsm-ida softhsm $DST_NS
+  $COPY_UTIL secret s3 s3 $DST_NS
+  $COPY_UTIL secret email-gateway msg-gateways $DST_NS
+  $COPY_UTIL secret mosip-captcha captcha $DST_NS
+  $COPY_UTIL secret conf-secrets-various conf-secrets $DST_NS
+  return 0
+}
