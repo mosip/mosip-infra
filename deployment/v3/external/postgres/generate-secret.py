8b9cf0fea618b506f5a31bb0302a31f5b309bb7d
Rakshithb1 <79500257+Rakshithb1@users.noreply.github.com>
2023-07-25 12:20:26 +0530

    [MOSIP-28519] add generate-secret.py and updated README.md (#1382)

---
+++ deployment/v3/external/postgres/generate-secret.py
@@ -0,0 +1,46 @@
+import base64
+
+# Prompt the user for the password
+password = input("Enter the db-dbuser-password: ")
+
+# Encode the password in base64
+db_commons_base64_password = base64.b64encode(password.encode()).decode()
+
+# Create the YAML content
+db_commons_yaml_content = f"""
+apiVersion: v1
+kind: Secret
+metadata:
+  name: db-common-secrets
+  namespace: postgres
+type: Opaque
+data:
+  db-dbuser-password: {db_commons_base64_password}
+"""
+
+# Write the YAML content to a file
+with open("db-common-secrets.yaml", "w") as file:
+    file.write(db_commons_yaml_content)
+
+
+# Prompt the user for the postgres password
+postgres_password = input("Enter postgres user password: ")
+
+# Encode the password in base64
+postgres_base64_password = base64.b64encode(postgres_password.encode()).decode()
+
+# Create the YAML content
+postgres_yaml_content = f"""
+apiVersion: v1
+kind: Secret
+metadata:
+  name: postgres-postgresql
+  namespace: postgres
+type: Opaque
+data:
+  postgresql-password: {postgres_base64_password}
+"""
+
+# Write the YAML content to a file
+with open("postgres-postgresql.yaml", "w") as file:
+    file.write(postgres_yaml_content)
