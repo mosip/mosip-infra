f91503a5e5c0ebfe25a68399849802941ee18de8
akilalakshmanan <Akila.L@iiitb.ac.in>
2023-08-24 11:48:57 +0530

    [DSD-3282] Updated image name

--- deployment/v3/mosip/biosdk/install.sh
+++ deployment/v3/mosip/biosdk/install.sh
@@ -22,7 +22,7 @@ function installing_biosdk() {
   ./copy_cm.sh
 
   echo Installing Biosdk server
-  helm -n $NS install biosdk-service mosip/biosdk-service -f values.yaml --set image.repository=mosipdev/biosdk-server --set image.tag=develop  --version $CHART_VERSION
+  helm -n $NS install biosdk-service mosip/biosdk-service -f values.yaml  --version $CHART_VERSION
 
   echo Biosdk service installed sucessfully.
   return 0
