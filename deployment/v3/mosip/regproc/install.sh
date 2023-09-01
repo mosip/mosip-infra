f75e5bd35b1047222d03453530d6b175e0b3b8b3
Rakshitha650 <rakshitham38@gmail.com>
2023-09-01 19:43:20 +0530

    [MOSIP-28149]updated as per the review comments

--- deployment/v3/mosip/regproc/install.sh
+++ deployment/v3/mosip/regproc/install.sh
@@ -2,7 +2,7 @@
 # Installs all regproc helm charts
 ## Usage: ./install.sh [kubeconfig]
 
-if [ $ -ge 1 ] ; then
+if [ $# -ge 1 ] ; then
   export KUBECONFIG=$1
 fi
 
@@ -75,10 +75,10 @@ function installing_regproc() {
   return 0
 }
 
- set commands for error handling.
+# set commands for error handling.
 set -e
-set -o errexit    set -e : exit the script if any statement returns a non-true return value
-set -o nounset    set -u : exit the script if you try to use an uninitialised variable
-set -o errtrace   trace ERR through 'time command' and other functions
-set -o pipefail   trace ERR through pipes
-installing_regproc    calling function
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_regproc   # calling function
