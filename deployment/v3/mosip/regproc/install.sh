550939ff2f87a723632d1eb7eba8b3cd2133c679
Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>
2023-09-05 21:00:02 +0530

    Update install.sh
    Signed-off-by: Rakshitha650 <76676196+Rakshitha650@users.noreply.github.com>

--- deployment/v3/mosip/regproc/install.sh
+++ deployment/v3/mosip/regproc/install.sh
@@ -21,53 +21,53 @@ function installing_regproc() {
   sed -i 's/\r$//' copy_cm.sh
   ./copy_cm.sh
 
-#  echo Running regproc-salt job
-#  helm -n $NS install regproc-salt mosip/regproc-salt --version $CHART_VERSION --wait --wait-for-jobs
+  echo Running regproc-salt job
+  helm -n $NS install regproc-salt mosip/regproc-salt --version $CHART_VERSION --wait --wait-for-jobs
 
-#  echo Installing regproc-workflow
-#  helm -n $NS install regproc-workflow mosip/regproc-workflow --version $CHART_VERSION
+  echo Installing regproc-workflow
+  helm -n $NS install regproc-workflow mosip/regproc-workflow --version $CHART_VERSION
 
-#  echo Installing regproc-status
-#  helm -n $NS install regproc-status mosip/regproc-status --version $CHART_VERSION
+  echo Installing regproc-status
+  helm -n $NS install regproc-status mosip/regproc-status --version $CHART_VERSION
 
-#  echo Installing regproc-camel
-#  helm -n $NS install regproc-camel mosip/regproc-camel --version $CHART_VERSION
+  echo Installing regproc-camel
+  helm -n $NS install regproc-camel mosip/regproc-camel --version $CHART_VERSION
 
-#  echo Installing regproc-pktserver
-#  helm -n $NS install regproc-pktserver mosip/regproc-pktserver --version $CHART_VERSION
+  echo Installing regproc-pktserver
+  helm -n $NS install regproc-pktserver mosip/regproc-pktserver --version $CHART_VERSION
 
   echo Installing group1
-  helm -n $NS install regproc-group1 /home/techno-376/IdeaProjects/mosip-helm/charts/regproc-group1 -f group1_values.yaml --version $CHART_VERSION
+  helm -n $NS install regproc-group1 mosip/regproc-group1 -f group1_values.yaml --version $CHART_VERSION
 
-#  echo Installing group2
-#  helm -n $NS install regproc-group2 mosip/regproc-group2  --version $CHART_VERSION
+  echo Installing group2
+  helm -n $NS install regproc-group2 mosip/regproc-group2  --version $CHART_VERSION
 
-#  echo Installing group3
-#  helm -n $NS install regproc-group3 mosip/regproc-group3  --version $CHART_VERSION
+  echo Installing group3
+  helm -n $NS install regproc-group3 mosip/regproc-group3  --version $CHART_VERSION
 
-#  echo Installing group4
-#  helm -n $NS install regproc-group4 mosip/regproc-group4 --version $CHART_VERSION
+  echo Installing group4
+  helm -n $NS install regproc-group4 mosip/regproc-group4 --version $CHART_VERSION
 
-#  echo Installing group5
-#  helm -n $NS install regproc-group5 mosip/regproc-group5 --version $CHART_VERSION
+  echo Installing group5
+  helm -n $NS install regproc-group5 mosip/regproc-group5 --version $CHART_VERSION
 
-#  echo Installing group6
-#  helm -n $NS install regproc-group6 mosip/regproc-group6 --version $CHART_VERSION
+  echo Installing group6
+  helm -n $NS install regproc-group6 mosip/regproc-group6 --version $CHART_VERSION
 
-#  echo Installing group7
-#  helm -n $NS install regproc-group7 mosip/regproc-group7 --version $CHART_VERSION
+  echo Installing group7
+  helm -n $NS install regproc-group7 mosip/regproc-group7 --version $CHART_VERSION
 
-#  echo Installing regproc-trans
-#  helm -n $NS install regproc-trans mosip/regproc-trans --version $CHART_VERSION
+  echo Installing regproc-trans
+  helm -n $NS install regproc-trans mosip/regproc-trans --version $CHART_VERSION
 
-#  echo Installing regproc-notifier
-#  helm -n $NS install regproc-notifier mosip/regproc-notifier --version $CHART_VERSION
+  echo Installing regproc-notifier
+  helm -n $NS install regproc-notifier mosip/regproc-notifier --version $CHART_VERSION
 
-#  echo Installing regproc-reprocess
-#  helm -n $NS install regproc-reprocess mosip/regproc-reprocess --version $CHART_VERSION
+  echo Installing regproc-reprocess
+  helm -n $NS install regproc-reprocess mosip/regproc-reprocess --version $CHART_VERSION
 
-#  echo Installing regproc-landingzone
-#  helm -n $NS install regproc-landingzone mosip/regproc-landingzone --version $CHART_VERSION
+  echo Installing regproc-landingzone
+  helm -n $NS install regproc-landingzone mosip/regproc-landingzone --version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
