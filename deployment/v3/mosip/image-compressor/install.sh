23cd253502be764b60c0766e80e104feaa72bf5c
Rakshithb1 <rakshit.b@technoforte.co.in>
2023-10-04 16:58:46 +0530

    [DSD-3532] added imagecompressor installation script

---
+++ deployment/v3/mosip/image-compressor/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Installs imagecompressor
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=image-compressor
+CHART_VERSION=12.0.2
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+function installing_imagecompressor() {
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
+  helm repo update
+
+  echo Copy configmaps
+  sed -i 's/\r$//' copy_cm.sh
+  ./copy_cm.sh
+
+  echo Installing imagecompressor server
+  helm -n $NS install image-compressor mosip/image-compressor -f values.yaml  --version $CHART_VERSION
+
+  echo imagecompressor service installed sucessfully.
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_imagecompressor   # calling function
