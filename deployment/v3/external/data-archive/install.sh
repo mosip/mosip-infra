9247c53c05747b07b9ac994ab088c832ca665196
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2023-11-17 16:57:10 +0530

    optimised install.sh
    Signed-off-by: bhumi46 <111699703+bhumi46@users.noreply.github.com>

--- deployment/v3/external/data-archive/install.sh
+++ deployment/v3/external/data-archive/install.sh
@@ -2,7 +2,7 @@
 # Installs data-archive
 ## Usage: ./install.sh [kubeconfig]
 
-if [ $# -ge 1 ] ; then
+if [ $# -ge 1 ]; then
   export KUBECONFIG=$1
 fi
 
@@ -16,7 +16,7 @@ function installing_data-archive() {
   echo Updating repos
   helm repo add mosip https://mosip.github.io/mosip-helm
   helm repo update
-  
+
   read -p "Is values.yaml for data-archive chart set correctly as part of Pre-requisites?(Y/n) " yn;
   if [ "$yn" != "Y" ]; then
     echo "ERROR: values.yaml not set correctly; EXITING;";
@@ -25,16 +25,16 @@ function installing_data-archive() {
 
   read -p "Please enter the time(hr) to run the cronjob every day (time: 0-23) : " time
   if [ -z "$time" ]; then
-     echo "ERROR: Time cannot be empty; EXITING;";
-     exit 1;
+    echo "ERROR: Time cannot be empty; EXITING;";
+    exit 1;
   fi
   if ! [ $time -eq $time ] 2>/dev/null; then
-     echo "ERROR: Time $time is not a number; EXITING;";
-     exit 1;
+    echo "ERROR: Time $time is not a number; EXITING;";
+    exit 1;
   fi
-  if [ $time -gt 23 ] || [ $time -lt 0 ] ; then
-     echo "ERROR: Time should be in range ( 0-23 ); EXITING;";
-     exit 1;
+  if [ $time -gt 23 ] || [ $time -lt 0 ]; then
+    echo "ERROR: Time should be in range ( 0-23 ); EXITING;";
+    exit 1;
   fi
 
   read -p "Is archival running for sandbox installation? (Y/N): " archival_running
@@ -43,7 +43,6 @@ function installing_data-archive() {
     super_user_password=$(kubectl get secret --namespace postgres postgres-postgresql -o jsonpath={.data.postgres-password} | base64 --decode)
     echo "Common secrets will be used as passwords for all the db users."
     db_common_password=$(kubectl get secret --namespace postgres db-common-secrets -o jsonpath={.data.db-dbuser-password} | base64 --decode)
-    echo Installing data-archive
     set_db_pwd="--set databases.archive_db.su_user_pwd=$super_user_password \
       --set databases.source_db.source_audit_db_pass=$db_common_password \
       --set databases.source_db.source_credential_db_pass=$db_common_password \
@@ -57,14 +56,17 @@ function installing_data-archive() {
       --set databases.source_db.source_resident_db_pass=$db_common_password \
       --set databases.archive_db.db_pwd=$db_common_password \
       --set databases.archive_db.archive_db_password=$db_common_password"
-    helm -n $NS install data-archive mosip/data-archive $set_db_pwd --set crontime="0 $time * * *" -f values.yaml --version $CHART_VERSION
+
   elif [ "$archival_running" == "N" ]; then
-    echo "Using individual secrets for db passwords"
-    helm -n $NS install data-archive mosip/data-archive  --set crontime="0 $time * * *" -f values.yaml --version $CHART_VERSION
+    echo "Other installation selected.This will Use individual secrets for db passwords from values.yaml"
+    set_db_pwd=""
   else
     echo "Incorrect input; EXITING;"
     exit 1;
   fi
+
+  # Install data-archive
+  helm -n $NS install data-archive /home/bhuminathan/da/mosip-helm/charts/data-archive --set crontime="0 $time * * *" -f values.yaml $set_db_pwd --version $CHART_VERSION
 
   echo Installed data-archive
   return 0
