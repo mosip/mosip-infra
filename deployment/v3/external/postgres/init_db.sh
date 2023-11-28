d5030ea132924d48d49f1741d773077f3081b571
syed salman <syedsalman3753@gmail.com>
2023-11-28 15:45:21 +0530

    [DSD-3884] Updated deployment as per esignet vc release
    Signed-off-by: syed salman <syedsalman3753@gmail.com>

--- deployment/v3/external/postgres/init_db.sh
+++ deployment/v3/external/postgres/init_db.sh
@@ -8,14 +8,14 @@ fi
 
 function initialize_db() {
   NS=postgres
-  CHART_VERSION=12.0.1-B3
+  CHART_VERSION=12.0-ES
   helm repo update
   while true; do
       read -p "CAUTION: all existing data will be lost. Are you sure?(Y/n)" yn
       if [ $yn = "Y" ]
         then
           echo Removing any existing installation
-          helm -n $NS delete postgres-init
+          helm -n $NS delete postgres-init || true
           echo Initializing DB
           helm -n $NS install postgres-init mosip/postgres-init -f init_values.yaml --version $CHART_VERSION --wait --wait-for-jobs
           break
