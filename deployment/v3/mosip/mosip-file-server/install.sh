aba6064bfaebfd640d3dc0b88bc4ee0b79d1b33b
abhishek8shankar <abhishek.shankarcs@gmail.com>
2024-01-17 19:03:53 +0530

    [DSD-4155] Changed configurations for camdgc-qa2
    Signed-off-by: abhishek8shankar <abhishek.shankarcs@gmail.com>

--- deployment/v3/mosip/mosip-file-server/install.sh
+++ deployment/v3/mosip/mosip-file-server/install.sh
@@ -32,7 +32,7 @@ function installing_mfs() {
 
   echo Install mosip-file-server. This may take a few minutes ..
   helm -n $NS install mosip-file-server mosip/mosip-file-server      \
-    --set image.repository=mosipqa/mosip-file-server --set image.tag=1.2.0.1 \
+    --set image.repository=mosipqa/mosip-file-server --set image.tag=develop \
     --set mosipfileserver.host=$FILESERVER_HOST                      \
     --set mosipfileserver.secrets.KEYCLOAK_CLIENT_SECRET="$KEYCLOAK_CLIENT_SECRET" \
     --set istio.corsPolicy.allowOrigins\[0\].prefix=https://$API_HOST \
