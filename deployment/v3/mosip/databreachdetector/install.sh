babd42b5a0d0edceeec5bb53a3db920d9e2ba055
VSIVAKALYAN <kalyanvellanki321@gmail.com>
2024-01-18 13:24:01 +0530

    [MOSIP-29854] Added install.sh script for databreachdetector
    Signed-off-by: VSIVAKALYAN <kalyanvellanki321@gmail.com>

---
+++ deployment/v3/mosip/databreachdetector/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Installs sample print service
+## Usage: ./restart.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+
+NS=databreachdetector
+CHART_VERSION=12.0.1-B2
+
+echo Create $NS namespace
+kubectl create ns $NS 
+
+function installing_databreachdetector() {
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
+  helm repo update
+
+  echo Copy configmaps
+  sed -i 's/\r$//' copy_cm.sh
+  ./copy_cm.sh
+
+  echo Installing databreachdetector
+  helm -n $NS install databreachdetector /home/siva/Mahesh_Helm_Chart_Creation/mosip-helm/charts/databreachdetector --set image.repository=maheshbinayak1/databreachdetector --set image.tag=latest --wait --version $CHART_VERSION
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_databreachdetector   # calling function
