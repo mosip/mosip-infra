6af3392b987ca91aa2d708825921e918fc55d353
shubham-technoforte <shubham.gawali@technoforte.co.in>
2024-01-23 14:01:37 +0530

    [DSD-4140] changed configurations for camdgc-perf

--- deployment/v3/mosip/idrepo/install.sh
+++ deployment/v3/mosip/idrepo/install.sh
@@ -22,16 +22,16 @@ function installing_idrepo() {
   ./copy_cm.sh
 
   echo Running salt generator job
-  helm -n $NS install idrepo-saltgen  mosip/idrepo-saltgen --set image.repository=mosipqa/id-repository-salt-generator --set image.tag=1.2.0.1-B1 --version $CHART_VERSION --wait --wait-for-jobs
+  helm -n $NS install idrepo-saltgen  mosip/idrepo-saltgen --set image.repository=mosipid/id-repository-salt-generator --set image.tag=1.2.0.1-B1 --version $CHART_VERSION --wait --wait-for-jobs
 
   echo Running credential
-  helm -n $NS install credential mosip/credential --set image.repository=mosipqa/credential-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
+  helm -n $NS install credential mosip/credential --set image.repository=mosipdev/credential-service --set image.tag=develop --version $CHART_VERSION
 
   echo Running credential request service
-  helm -n $NS install credentialrequest mosip/credentialrequest --set image.repository=mosipqa/credential-request-generator --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
+  helm -n $NS install credentialrequest mosip/credentialrequest --set image.repository=mosipdev/credential-request-generator --set image.tag=develop --version $CHART_VERSION
 
   echo Running identity service
-  helm -n $NS install identity mosip/identity --set image.repository=mosipqa/id-repository-identity-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
+  helm -n $NS install identity mosip/identity --set image.repository=mosipdev/id-repository-identity-service --set image.tag=develop --version $CHART_VERSION
 
   #echo Running vid service
   #helm -n $NS install vid mosip/vid --version $CHART_VERSION
--- deployment/v3/mosip/kernel/install.sh
+++ deployment/v3/mosip/kernel/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=kernel
-CHART_VERSION=12.0.1-B2
+CHART_VERSION=12.0.2
 
 echo Create $NS namespace
 kubectl create ns $NS
@@ -22,20 +22,20 @@ function installing_kernel() {
   ./copy_cm.sh
 
   echo Installing authmanager
-  helm -n $NS install authmanager mosip/authmanager --set image.repository=mosipqa/kernel-auth-service --set image.tag=1.2.0.1-B2 --version $CHART_VERSION
+  helm -n $NS install authmanager mosip/authmanager --set image.repository=mosipid/kernel-auth-service --set image.tag=1.2.0.1-B2 --version $CHART_VERSION
 
   echo Installing auditmanager
   helm -n $NS install auditmanager mosip/auditmanager --set image.repository=mosipqa/kernel-auditmanager-service --set image.tag=1.2.0.1 --version $CHART_VERSION
 
   echo Installing idgenerator
-  helm -n $NS install idgenerator mosip/idgenerator --set image.repository=mosipqa/kernel-idgenerator-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
+  helm -n $NS install idgenerator mosip/idgenerator --set image.repository=mosipid/kernel-idgenerator-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
 
   ADMIN_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-admin-host})
   echo Installing masterdata and allowing Admin UI to access masterdata services.
-  helm -n $NS install masterdata mosip/masterdata --set image.repository=mosipqa/kernel-masterdata-service --set image.tag=1.2.0.1-B1  --set istio.corsPolicy.allowOrigins\[0\].exact=https://$ADMIN_HOST  --version $CHART_VERSION
+  helm -n $NS install masterdata mosip/masterdata --set image.repository=mosipid/kernel-masterdata-service --set image.tag=1.2.0.1-B1  --set istio.corsPolicy.allowOrigins\[0\].exact=https://$ADMIN_HOST  --version $CHART_VERSION
 
   echo Installing otpmanager
-  helm -n $NS install otpmanager mosip/otpmanager --set image.repository=mosipqa/kernel-otpmanager-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
+  helm -n $NS install otpmanager mosip/otpmanager --set image.repository=mosipid/kernel-otpmanager-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
 
   #echo Installing pridgenerator
   #helm -n $NS install pridgenerator mosip/pridgenerator --version $CHART_VERSION
@@ -47,7 +47,7 @@ function installing_kernel() {
   #helm -n $NS install syncdata mosip/syncdata --version $CHART_VERSION
 
   echo Installing notifier
-  helm -n $NS install notifier mosip/notifier --set image.repository=mosipqa/kernel-notification-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
+  helm -n $NS install notifier mosip/notifier --set image.repository=mosipid/kernel-notification-service --set image.tag=1.2.0.1-B1 --version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
 
