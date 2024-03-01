20c4dfb697ac6de1670c03b79864b1df0bb4a178
syed salman <syedsalman3753@gmail.com>
2024-03-01 16:22:43 +0530

    [DSD-4728] Added fetch-images-deployed-in-k8s-cluster.sh
    Signed-off-by: syed salman <syedsalman3753@gmail.com>

--- deployment/v3/testrig/apitestrig/install.sh
+++ deployment/v3/testrig/apitestrig/install.sh
@@ -101,6 +101,7 @@ function installing_apitestrig() {
   --set apitestrig.configmaps.apitestrig.ENV_TESTLEVEL="smokeAndRegression" \
   --set apitestrig.configmaps.apitestrig.reportExpirationInDays="$reportExpirationInDays" \
   --set apitestrig.configmaps.apitestrig.eSignetDeployed="$eSignetDeployed" \
+  --set apitestrig.configmaps.apitestrig.NS="$NS" \
   $ENABLE_INSECURE
   
   echo Installed apitestrig.
