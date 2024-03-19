9948738c11db0fec13c449e64a2159c6bf6355c5
abhishek8shankar <abhishek.shankarcs@gmail.com>
2024-03-19 17:47:40 +0530

    [DSD-4835] Updated Images as per requirment
    Signed-off-by: abhishek8shankar <abhishek.shankarcs@gmail.com>

--- deployment/v3/mosip/masterdata-loader/install.sh
+++ deployment/v3/mosip/masterdata-loader/install.sh
@@ -12,7 +12,7 @@ read -p "CAUTION: Do you still want to continue(Y/n)" yn
 if [ $yn = "Y" ]
   then
    NS=masterdata-loader
-   CHART_VERSION=12.0.1-B2
+   CHART_VERSION=12.0.1
    helm delete masterdata-loader -n $NS
    echo Create $NS namespace
    kubectl create ns $NS
@@ -33,7 +33,7 @@ if [ $yn = "Y" ]
    ./copy_secrets.sh
 
    echo Loading masterdata
-   helm -n $NS install masterdata-loader  mosip/masterdata-loader --set image.repository=mosipqa/masterdata-loader --set image.tag=develop --set mosipDataGithubBranch=1.2.0.1 --version $CHART_VERSION --wait
+   helm -n $NS install masterdata-loader  mosip/masterdata-loader --set image.repository=mosipid/masterdata-loader --set image.tag=1.2.0.1 --set mosipDataGithubBranch=1.2.0.1 --version $CHART_VERSION --wait
 
    else
    break
--- deployment/v3/mosip/websub/install.sh
+++ deployment/v3/mosip/websub/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=websub
-CHART_VERSION=12.0.1-B2
+CHART_VERSION=12.0.1
 
 echo Create $NS namespace
 kubectl create ns $NS
@@ -22,8 +22,8 @@ function installing_websub() {
   ./copy_cm.sh
 
   echo Installing websub
-  helm -n $NS install websub-consolidator mosip/websub-consolidator --set image.repository=mosipqa/consolidator-websub-service --set image.tag=develop --version $CHART_VERSION --wait
-  helm -n $NS install websub mosip/websub --set image.repository=mosipqa/websub-service --set image.tag=develop --version $CHART_VERSION
+  helm -n $NS install websub-consolidator mosip/websub-consolidator --set image.repository=mosipid/consolidator-websub-service --set image.tag=1.2.0.1 --version $CHART_VERSION --wait
+  helm -n $NS install websub mosip/websub --set image.repository=mosipid/websub-service --set image.tag=1.2.0.1 --version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
   echo Installed websub services
