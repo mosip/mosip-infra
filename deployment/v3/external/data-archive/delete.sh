061b145cccd08fa9f24062fa2e8b385f268fba2d
bhumi46 <thisisbn46@gmail.com>
2024-03-21 13:58:23 +0530

    [MOSIP-27423]
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

---
+++ deployment/v3/external/data-archive/delete.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Uninstalls data-archive
+## Usage: ./delete.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+function deleting_data-archive() {
+  NS=data-archive
+  while true; do
+      read -p "Are you sure you want to delete data-archive helm charts?(Y/n) " yn
+      if [ $yn = "Y" ]
+        then
+          helm -n $NS delete data-archive
+          break
+        else
+          break
+      fi
+  done
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+deleting_data-archive   # calling function
