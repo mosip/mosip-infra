31bdcf9ff2bbaee1a81157d8c5ec907eb770f361
syed salman <72004356+syedsalman3753@users.noreply.github.com>
2024-07-11 14:02:43 +0530

    [MOSIP-34312] Remove onboarder-namespace configmaps if exists
    Signed-off-by: syed salman <72004356+syedsalman3753@users.noreply.github.com>

--- deployment/v3/mosip/partner-onboarder/install.sh
+++ deployment/v3/mosip/partner-onboarder/install.sh
@@ -36,6 +36,7 @@ function installing_onboarder() {
 
     echo Copy configmaps
     kubectl -n $NS --ignore-not-found=true delete cm s3
+    kubectl -n $NS --ignore-not-found=true delete cm onboarder-namespace
     sed -i 's/\r$//' copy_cm.sh
     ./copy_cm.sh
 
