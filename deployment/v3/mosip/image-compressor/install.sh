071f1e8445fef3447059109e7f4d147df7e3e666
Rakshith B <79500257+Rakshithb1@users.noreply.github.com>
2024-08-13 13:33:08 +0530

    [DSD-3532] Updated install.sh
    Signed-off-by: Rakshith B <79500257+Rakshithb1@users.noreply.github.com>

--- deployment/v3/mosip/image-compressor/install.sh
+++ deployment/v3/mosip/image-compressor/install.sh
@@ -23,7 +23,7 @@ function installing_imagecompressor() {
 
   echo Installing imagecompressor server
   helm -n $NS install image-compressor mosip/biosdk-service \
-  --set extraEnvVars[0].name="server_servlet_context_env" \
+  --set extraEnvVars[0].name="service_context_env" \
   --set extraEnvVars[0].value="/image-compressor" \
   --set extraEnvVars[1].name="spring_application_name_env" \
   --set extraEnvVars[1].value="image-compressor" \
