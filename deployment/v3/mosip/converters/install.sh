8c961e6fa98230f7b96c9643e4f32bbc33fb9c09
Rakshithb1 <rakshit.b@technoforte.co.in>
2024-11-12 16:55:23 +0530

    [MOSIP-37407] added installation scripts
    Signed-off-by: Rakshithb1 <rakshit.b@technoforte.co.in>

---
+++ deployment/v3/mosip/converters/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Installs all converters helm charts
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=converters
+CHART_VERSION=0.0.1-develop
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+function installing_converters() {
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
+  helm repo update
+
+  echo Copy configmaps
+  sed -i 's/\r$//' copy_cm.sh
+  ./copy_cm.sh
+
+  echo Installing converters
+  helm -n $NS install converters mosip/converters --version $CHART_VERSION
+
+  kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
+
+  echo Installed converters services
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_converters   # calling function
