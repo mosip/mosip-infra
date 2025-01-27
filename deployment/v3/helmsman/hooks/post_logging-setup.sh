1586e9cd70bb1dfb96b16f5f83420fca64b5f8fd
Mohanraj209 <mohan1715@gmail.com>
2025-01-27 16:05:03 +0530

    [MOSIP-39077] parametrise and add customisation support to external and pre-requisites helmsman automation.
    Signed-off-by: Mohanraj209 <mohan1715@gmail.com>

--- deployment/v3/helmsman/hooks/post_logging-setup.sh
+++ deployment/v3/helmsman/hooks/post_logging-setup.sh
@@ -36,11 +36,11 @@ function post_logging_setup() {
   }'
 
   echo "Configure Rancher FluentD"
-  kubectl apply -f ./utils/logging/clusteroutput-elasticsearch.yaml
-  kubectl apply -f ./utils/logging/clusterflow-elasticsearch.yaml
+  kubectl apply -f ../utils/logging/clusteroutput-elasticsearch.yaml
+  kubectl apply -f ../utils/logging/clusterflow-elasticsearch.yaml
 
   echo "Load Dashboards"
-  ./utils/logging/load_kibana_dashboards.sh ./utils/logging/dashboards ~/.kube/soil.config
+  ../utils/logging/load_kibana_dashboards.sh ../utils/logging/dashboards ~/.kube/config
   echo "Dashboards loaded"
   return 0
 }
