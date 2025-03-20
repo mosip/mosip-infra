a79cd98e8a254b895cb93b478029209e090746d6
Mohan E <mohanraj1715@gmail.com>
2025-03-20 11:49:56 +0530

    Update install-istio-and-httpbin.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- deployment/v3/helmsman/hooks/install-istio-and-httpbin.sh
+++ deployment/v3/helmsman/hooks/install-istio-and-httpbin.sh
@@ -17,8 +17,9 @@ function installing_istio_and_httpbin() {
   echo "Installing Global Configmap"
   pwd
   chmod +x deployment/v3/helmsman/utils/global_configmap.yaml
-  envsubst < deployment/v3/helmsman/utils/global_configmap.yaml > global_configmap.generated.yaml
-  kubectl apply -f ./global_configmap_generated.yaml
+  envsubst < deployment/v3/helmsman/utils/global_configmap.yaml > ./global_configmap.generated.yaml
+  ls
+  kubectl apply -f global_configmap_generated.yaml
   echo "Installed Global Configmap"
 
   echo Create ingress gateways, load balancers and istio monitoring
