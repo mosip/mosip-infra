478623c036c4bb2d56c69873cec3be4c85f85824
Mohan E <mohanraj1715@gmail.com>
2025-04-15 16:02:23 +0530

    Updated regproc-setup.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- deployment/v3/helmsman/hooks/regproc-setup.sh
+++ deployment/v3/helmsman/hooks/regproc-setup.sh
@@ -5,7 +5,7 @@ NS=regproc
 function installing_regproc() {
 
   echo Copying Resorces
-  COPY_UTIL=../utils/copy-cm-and-secrets/copy_cm_func.sh
+  COPY_UTIL=$WORKDIR/utils/copy-cm-and-secrets/copy_cm_func.sh
   #Copy configmaps
   $COPY_UTIL configmap global default $NS
   $COPY_UTIL configmap artifactory-share artifactory $NS
