b7b8417183e4055daeac74c706f03a354b7045b5
Mohan E <mohanraj1715@gmail.com>
2025-04-23 23:33:53 +0530

    Updated wait-for-keygen.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- deployment/v3/helmsman/hooks/wait-for-keygen.sh
+++ deployment/v3/helmsman/hooks/wait-for-keygen.sh
@@ -2,5 +2,5 @@
 # wait-for-keygen.sh
 until kubectl get job kernel-keygen -n keymanager -o jsonpath='{.status.succeeded}' | grep 1; do
   echo "Waiting for keygen job to complete..."
-  sleep 120
+  sleep 10
 done
