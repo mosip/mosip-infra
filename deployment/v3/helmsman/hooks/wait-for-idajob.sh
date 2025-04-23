36b6e2d7aff319b1d0c2a3eb063a2967bafcd328
Mohan E <mohanraj1715@gmail.com>
2025-04-23 23:30:35 +0530

    Updated wait-for-idajob.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- deployment/v3/helmsman/hooks/wait-for-idajob.sh
+++ deployment/v3/helmsman/hooks/wait-for-idajob.sh
@@ -2,6 +2,5 @@
 # wait-for-keygen.sh
 until kubectl get job ida-keygen -n ida -o jsonpath='{.status.succeeded}' | grep 1; do
   echo "Waiting for keygen job to complete..."
-  sleep 120
+  sleep 10
 done
-kubectl label ns ida istio-injection=enabled --overwrite
