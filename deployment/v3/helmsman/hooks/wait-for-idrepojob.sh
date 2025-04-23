712f5d12c79290cd0db3a531f795900da4062f98
Mohan E <mohanraj1715@gmail.com>
2025-04-23 23:32:32 +0530

    Updated wait-for-idrepojob.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- deployment/v3/helmsman/hooks/wait-for-idrepojob.sh
+++ deployment/v3/helmsman/hooks/wait-for-idrepojob.sh
@@ -2,6 +2,5 @@
 # wait-for-keygen.sh
 until kubectl get job idrepo-saltgen -n idrepo -o jsonpath='{.status.succeeded}' | grep 1; do
   echo "Waiting for keygen job to complete..."
-  sleep 120
+  sleep 10
 done
-kubectl label ns idrepo istio-injection=enabled --overwrite
