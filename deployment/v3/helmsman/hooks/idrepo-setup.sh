58e3e213c28327af699da3900d29ed45c16377fd
Mohan E <mohanraj1715@gmail.com>
2025-04-23 23:33:14 +0530

    Updated idrepo-setup.sh
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- deployment/v3/helmsman/hooks/idrepo-setup.sh
+++ deployment/v3/helmsman/hooks/idrepo-setup.sh
@@ -3,6 +3,9 @@
 
 NS=idrepo
 function installing_idrepo() {
+
+  kubectl label ns $NS istio-injection=enabled --overwrite
+
   echo Copying Resorces
   COPY_UTIL=$WORKDIR/utils/copy-cm-and-secrets/copy_cm_func.sh
   #Copy configmaps
