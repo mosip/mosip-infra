fb526cf63eb3bbd5da8f6d61b5a6b51988e0c848
Swapnil <swapnil.mohanty@technoforte.co.in>
2025-12-05 18:53:53 +0530

    Update install and copy_secrets script
    Signed-off-by: Swapnil <swapnil.mohanty@technoforte.co.in>

---
+++ deployment/v3/mosip/mosip-file-server/copy_secrets.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copy secrets from other namespaces
+# DST_NS: Destination namespace
+
+function copying_secrets() {
+  UTIL_URL=https://raw.githubusercontent.com/mosip/mosip-infra/refs/heads/master/deployment/v3/utils/copy_cm_func.sh
+  COPY_UTIL=./copy_cm_func.sh
+  DST_NS=mosip-file-server
+
+  wget -q $UTIL_URL -O copy_cm_func.sh && chmod +x copy_cm_func.sh
+
+  $COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+copying_secrets   # calling function
