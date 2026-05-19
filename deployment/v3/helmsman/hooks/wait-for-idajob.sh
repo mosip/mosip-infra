ab25f07564406879cb407e61233a2e69809d91d0
bhumi46 <thisisbn46@gmail.com>
2026-05-19 19:03:58 +0530

    [MOSIP-44258]Removing Helmsman configs
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- deployment/v3/helmsman/hooks/wait-for-idajob.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# wait-for-keygen.sh
-until kubectl get job ida-keygen -n ida -o jsonpath='{.status.succeeded}' | grep 1; do
-  echo "Waiting for keygen job to complete..."
-  sleep 10
-done
--- deployment/v3/helmsman/hooks/wait-for-keygen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# wait-for-keygen.sh
-until kubectl get job kernel-keygen -n keymanager -o jsonpath='{.status.succeeded}' | grep 1; do
-  echo "Waiting for keygen job to complete..."
-  sleep 10
-done
